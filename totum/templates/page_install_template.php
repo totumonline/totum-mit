982e42e50a83cb4e820a8ec5700552bb7b960f8a
Tatiana <poluninat.m@gmail.com>
2019-11-20 17:03:40 +0300

    Изменить внешний вид панелей #80

--- totum/templates/page_install_template.php
+++ totum/templates/page_install_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=d005f67"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=f87723f">
+          href="/css/main.css?v=9555695">
 
     <?if(\totum\common\Auth::isCreator()){?>
         <script src="/js/functions.json?v=4cf768a"></script>
@@ -16,7 +16,7 @@
         }?>
     <?}?>
 
-    <script src="/js/main.js?v=dab72b2"></script>
+    <script src="/js/main.js?v=cc0d62f"></script>
 
 
 

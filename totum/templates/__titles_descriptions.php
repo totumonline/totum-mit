6a8f8ec0e0d211f7fd39037a9a794fc371c32700
Tatiana <poluninat.m@gmail.com>
2020-09-30 18:41:18 +0300

    change default title and description

--- totum/templates/__titles_descriptions.php
+++ totum/templates/__titles_descriptions.php
@@ -1,9 +1,9 @@
 <?php
 $host = 'http' . (!empty($_SERVER['HTTPS']) ? 's' : '') . '://' . $_SERVER['HTTP_HOST'] . '/';
-$settings['h_og_title'] = $settings['h_og_title'] ?? 'TOTUM — платформа для любой автоматизации в малом бизнесе';
+$settings['h_og_title'] = $settings['h_og_title'] ?? 'Totum — конструктор CRM, ERP, XRM.';
 if (empty($settings['h_title']))
     $settings['h_title'] = 'TOTUM';
-$settings['h_og_description'] = $settings['h_og_description'] ?? 'На ней можно собирать заточенные под клиента базы данных, специальные CRM, склады, расчеты, формы, калькуляторы и любой другой учет.';
+$settings['h_og_description'] = $settings['h_og_description'] ?? 'Разрабатывайте управленческие инструменты х5 раз быстрее.';
 $settings['h_og_image'] = empty($settings['h_og_image'][0]['file']) ? 'imgs/hand.png' : 'fls/' . $settings['h_og_image'][0]['file'];
 ?>
 <title><?= (empty($title) ? '' : $title . ' — ') . $settings['h_title'] ?></title>

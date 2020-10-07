856ff7a9fa4d65c6f679c61e6d36bb11b9ed0fb9
Tatiana <poluninat.m@gmail.com>
2020-10-07 17:21:55 +0300

    fx Table frontend errors

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -22,7 +22,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=0169ead"></script>
+    <script src="/js/main.js?v=be7913e"></script>
 
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=ebabb90">
-    <script src="/js/main.js?v=0169ead"></script>
+    <script src="/js/main.js?v=be7913e"></script>
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
 

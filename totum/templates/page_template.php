4699f4a9d9ec83a5f4d8ca7a3aa2fb59e7a6fe99
Tatiana <poluninat.m@gmail.com>
2020-10-14 17:27:17 +0300

    fx notitle height

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -22,7 +22,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=44f5758"></script>
+    <script src="/js/main.js?v=e311b2e"></script>
 
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>

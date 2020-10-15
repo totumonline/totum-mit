fb6a728ff5280ad2f965250af9e6b97dbb232dc8
Tatiana <poluninat.m@gmail.com>
2020-10-15 18:37:11 +0300

    fx fx buttons

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=ccefc80">
-    <script src="/js/main.js?v=3f53c90"></script>
+    <script src="/js/main.js?v=df0e097"></script>
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
 

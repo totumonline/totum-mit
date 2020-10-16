4dc1304235578ce00e04579dfa36a8ad421fc270
Tatiana <poluninat.m@gmail.com>
2020-10-16 13:35:32 +0300

    fx — в хедере подзалипает Выполнено на кнопке

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -22,7 +22,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=6cfcc2c"></script>
+    <script src="/js/main.js?v=13d156e"></script>
 
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=ccefc80">
-    <script src="/js/main.js?v=6cfcc2c"></script>
+    <script src="/js/main.js?v=13d156e"></script>
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
 

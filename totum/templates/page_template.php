8d75290228e7073149f718d00965f20887f48e6b
Tatiana <poluninat.m@gmail.com>
2020-10-19 15:20:22 +0300

    fx js число обрабатывало regexp для пустого значения

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -22,7 +22,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=be7c31b"></script>
+    <script src="/js/main.js?v=5555fc2"></script>
 
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=ccefc80">
-    <script src="/js/main.js?v=be7c31b"></script>
+    <script src="/js/main.js?v=5555fc2"></script>
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
 

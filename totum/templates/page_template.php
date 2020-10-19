1d937822878fbe093b6abe19070dd20c3e11c4b7
Tatiana <poluninat.m@gmail.com>
2020-10-19 17:47:31 +0300

    fx refresh hidden fields

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -22,7 +22,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=5555fc2"></script>
+    <script src="/js/main.js?v=1d321b2"></script>
 
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>

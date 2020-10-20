b49d0278f13084aca7417c13b1c2a2994490b21f
Tatiana <poluninat.m@gmail.com>
2020-10-20 15:44:21 +0300

    fx — техуведомление про недопустимый name для админ-панели открытой из ифрейма оказывается ниже панели

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -22,7 +22,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=cad95f5"></script>
+    <script src="/js/main.js?v=b3fb441"></script>
 
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=ccefc80">
-    <script src="/js/main.js?v=cad95f5"></script>
+    <script src="/js/main.js?v=b3fb441"></script>
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
 

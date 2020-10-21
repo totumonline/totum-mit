2111cb1775f80278b84998a62973a93d44763690
Tatiana <poluninat.m@gmail.com>
2020-10-21 18:04:59 +0300

    fx глаз

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -22,7 +22,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=167cc2a"></script>
+    <script src="/js/main.js?v=aa7fed6"></script>
 
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=ccefc80">
-    <script src="/js/main.js?v=167cc2a"></script>
+    <script src="/js/main.js?v=aa7fed6"></script>
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
 

c596dc0d55eb489623f68098d595d58e8e929ef2
Tatiana <poluninat.m@gmail.com>
2020-10-30 14:59:11 +0300

    fx самопроизвольный переход фокуса в строке добавления

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -22,7 +22,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=a4904e1"></script>
+    <script src="/js/main.js?v=31cafa2"></script>
 
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>

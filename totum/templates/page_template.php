c94ca52ad93915bb0552e2b3d2404ee041e6b5e9
Tatiana <poluninat.m@gmail.com>
2020-10-30 16:08:10 +0300

    improve front

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=1afab8b"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=ce96969">
+          href="/css/main.css?v=772118f">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -22,7 +22,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=31cafa2"></script>
+    <script src="/js/main.js?v=2860b22"></script>
 
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=1afab8b"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=ce96969">
-    <script src="/js/main.js?v=31cafa2"></script>
+          href="/css/main.css?v=772118f">
+    <script src="/js/main.js?v=2860b22"></script>
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
 

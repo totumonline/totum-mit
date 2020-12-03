4691b5d2ceac49c6e6a7413ce1c6718439c3111e
Tatiana <poluninat.m@gmail.com>
2020-12-03 12:27:48 +0300

    treeView fx add

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -22,7 +22,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=056c705"></script>
+    <script src="/js/main.js?v=e5dea6c"></script>
 
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=ca4d191">
-    <script src="/js/main.js?v=056c705"></script>
+    <script src="/js/main.js?v=e5dea6c"></script>
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
 

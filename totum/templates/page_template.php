a4689233a87b99e87e4353d9d697a7c01b698461
Tatiana <poluninat.m@gmail.com>
2020-12-08 15:47:50 +0300

    fx treeView problems

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -22,7 +22,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=2bf2558"></script>
+    <script src="/js/main.js?v=0455a01"></script>
 
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=37b5ae6">
-    <script src="/js/main.js?v=2bf2558"></script>
+    <script src="/js/main.js?v=0455a01"></script>
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
 

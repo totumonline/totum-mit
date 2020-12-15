40e7a61d4932e40cb54012c12bfc32cede049331
Tatiana <poluninat.m@gmail.com>
2020-12-15 14:58:26 +0300

    kanban td view in card

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -22,7 +22,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=98512a9"></script>
+    <script src="/js/main.js?v=3074a97"></script>
 
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=a802432">
-    <script src="/js/main.js?v=98512a9"></script>
+    <script src="/js/main.js?v=3074a97"></script>
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
 

3760dde683a3e72b8439e1b55c9c9f785a20e34d
Tatiana <poluninat.m@gmail.com>
2020-12-29 13:10:47 +0300

    fx treeView error; version 1.0.03

--- totum/common/Totum.php
+++ totum/common/Totum.php
@@ -26,7 +26,7 @@ use totum\tableTypes\tmpTable;
  */
 class Totum
 {
-    public const VERSION = '1.0.02';
+    public const VERSION = '1.0.03';
 
 
     public const TABLE_CODE_PARAMS = ['row_format', 'table_format'];
--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -22,7 +22,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=7eba902"></script>
+    <script src="/js/main.js?v=80531a3"></script>
 
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>

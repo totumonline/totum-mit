f0fd4c3eb17a09e399f00f73320e604c7360f241
Tatiana <poluninat.m@gmail.com>
2021-01-13 16:11:38 +0300

    show panel card go to cycle in panelsView

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -22,7 +22,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=9cc9181"></script>
+    <script src="/js/main.js?v=9ef0724"></script>
 
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=ac513bb">
-    <script src="/js/main.js?v=9cc9181"></script>
+    <script src="/js/main.js?v=9ef0724"></script>
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
 

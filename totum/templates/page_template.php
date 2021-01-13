92260709f7f09b53f8a9eaccc9587b517fb7289d
Tatiana <poluninat.m@gmail.com>
2021-01-13 16:19:50 +0300

    fx row card - CodeActionOnClick button

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -22,7 +22,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=9ef0724"></script>
+    <script src="/js/main.js?v=8f5599e"></script>
 
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=ac513bb">
-    <script src="/js/main.js?v=9ef0724"></script>
+    <script src="/js/main.js?v=8f5599e"></script>
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
 

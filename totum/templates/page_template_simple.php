91ee79aebd88f49639bacd3951e397b876a2c4eb
Tatiana <poluninat.m@gmail.com>
2021-01-15 17:17:29 +0300

    fx front

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=1afab8b"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=bf10418">
-    <script src="/js/main.js?v=b060a83"></script>
+          href="/css/main.css?v=65bc850">
+    <script src="/js/main.js?v=4c0f198"></script>
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
 

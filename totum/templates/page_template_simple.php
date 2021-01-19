1765c829f8732bb15dc37cd013e6fa93ae174a96
Tatiana <poluninat.m@gmail.com>
2021-01-19 16:49:38 +0300

    fx nSorting

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=f2cc046">
-    <script src="/js/main.js?v=67fa920"></script>
+    <script src="/js/main.js?v=3b91b9f"></script>
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
 

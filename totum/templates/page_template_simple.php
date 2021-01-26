865f647277c4b12cf83569b9ff026ca8cc5d1d6f
Tatiana <poluninat.m@gmail.com>
2021-01-26 13:30:35 +0300

    button edit select table row in right-click-panel

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=cc5cdda">
-    <script src="/js/main.js?v=b9f6b7f"></script>
+    <script src="/js/main.js?v=0343a80"></script>
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
 

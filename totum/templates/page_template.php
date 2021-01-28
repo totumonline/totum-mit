3541619cbad57ea86c2006de521af8ac95e8f5da
Tatiana <poluninat.m@gmail.com>
2021-01-28 17:26:23 +0300

    fx front error in Safari; version 1.0.11

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -22,7 +22,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=b031ecd"></script>
+    <script src="/js/main.js?v=72a1a4c"></script>
 
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>

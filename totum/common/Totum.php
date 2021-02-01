80afb3d2c4709583399d1755748f439a5f3b9c11
Tatiana <poluninat.m@gmail.com>
2021-02-01 15:54:24 +0300

    version 1.0.12

--- totum/common/Totum.php
+++ totum/common/Totum.php
@@ -26,7 +26,7 @@ use totum\tableTypes\tmpTable;
  */
 class Totum
 {
-    public const VERSION = '1.0.11';
+    public const VERSION = '1.0.12';
 
 
     public const TABLE_CODE_PARAMS = ['row_format', 'table_format'];

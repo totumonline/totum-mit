20fb1f512de0aad39ab066a9bd21b2bfe5eb0389
Tatiana <poluninat.m@gmail.com>
2021-02-02 16:34:42 +0300

    — при hide: "extpanel" = true при оставшейся единственной плашке, она не разъезжается в ширину

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -22,7 +22,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=909b5ea"></script>
+    <script src="/js/main.js?v=e0e1c34"></script>
 
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>

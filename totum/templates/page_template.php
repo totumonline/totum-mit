e7f26e0729363ece9107e6a34afd5eadceebfc19
Tatiana <poluninat.m@gmail.com>
2021-02-04 13:29:24 +0300

    fx если в окне перейти в еще одно окно, потом его закрыть и сказать Вкладка, то откроется закрытое окно

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -22,7 +22,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=4086907"></script>
+    <script src="/js/main.js?v=85310f3"></script>
 
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>

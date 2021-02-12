5980a643b7207aa6e9bd8cb21eb0b9a68552a16c
Tatiana <poluninat.m@gmail.com>
2021-02-12 16:04:51 +0300

    codemirror подсветка

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -22,7 +22,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=76244a6"></script>
+    <script src="/js/main.js?v=eb04745"></script>
 
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>

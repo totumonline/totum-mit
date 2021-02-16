2530a56d01fb4b5eca3e2d6d89b95a11a0538270
Tatiana <poluninat.m@gmail.com>
2021-02-16 14:11:32 +0300

    Float settings in section title

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -22,7 +22,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=c87920f"></script>
+    <script src="/js/main.js?v=7ca5de5"></script>
 
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=3750b22">
-    <script src="/js/main.js?v=c87920f"></script>
+    <script src="/js/main.js?v=7ca5de5"></script>
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
 

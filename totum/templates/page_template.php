3201ea83f0456d7832d1272f865a26a44af22762
Tatiana <poluninat.m@gmail.com>
2021-03-05 14:15:58 +0300

    codemirror fx

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -22,7 +22,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=9229b11"></script>
+    <script src="/js/main.js?v=e4788c7"></script>
 
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>

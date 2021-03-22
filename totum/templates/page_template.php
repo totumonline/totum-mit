63062cf91213a54150bebaa9f84810d2f504dfae
Tatiana <poluninat.m@gmail.com>
2021-03-22 17:08:36 +0300

    tablecomment correct width with tablebuttons

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=f52b4ad"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=d16a7d0">
+          href="/css/main.css?v=937cbbf">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=4815bee"></script>
+    <script src="/js/main.js?v=16380ae"></script>
 
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=f52b4ad"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=d16a7d0">
-    <script src="/js/main.js?v=4815bee"></script>
+          href="/css/main.css?v=937cbbf">
+    <script src="/js/main.js?v=16380ae"></script>
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
 

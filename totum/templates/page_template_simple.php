e88076718495df25566b5051446e4eadc8f9b797
Tatiana <poluninat.m@gmail.com>
2021-03-23 12:36:32 +0300

    empty kanban

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=f52b4ad"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=937cbbf">
-    <script src="/js/main.js?v=16380ae"></script>
+          href="/css/main.css?v=3ee3e93">
+    <script src="/js/main.js?v=3bca9cb"></script>
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
 

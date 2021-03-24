cfe6046438911f3b83f4429052be68ffc483d785
Tatiana <poluninat.m@gmail.com>
2021-03-24 16:17:05 +0300

    Codemirror font-size, summarizer decimal delimiter -> .

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=f52b4ad"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=0f76df6">
+          href="/css/main.css?v=dc96ff6">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=dd28014"></script>
+    <script src="/js/main.js?v=927d685"></script>
 
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>

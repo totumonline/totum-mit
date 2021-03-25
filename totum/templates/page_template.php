58e9a6b6dfc1105a6216ddbb5eaf652369bf81e0
Tatiana <poluninat.m@gmail.com>
2021-03-25 15:20:18 +0300

    codemirror fonts, border updates

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=f52b4ad"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=dc96ff6">
+          href="/css/main.css?v=5613407">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=cf7d710"></script>
+    <script src="/js/main.js?v=7483469"></script>
 
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=f52b4ad"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=dc96ff6">
-    <script src="/js/main.js?v=cf7d710"></script>
+          href="/css/main.css?v=5613407">
+    <script src="/js/main.js?v=7483469"></script>
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
 

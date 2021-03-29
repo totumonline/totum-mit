1a5e4a1dd038da7be4fb0307988ff890a16ac5e8
Tatiana <poluninat.m@gmail.com>
2021-03-29 14:58:12 +0300

    fx group editing

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=de6e836"></script>
+    <script src="/js/main.js?v=2790e4f"></script>
 
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>

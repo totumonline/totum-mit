f23dc18e5876f39ecf7b647343b47c687971918d
Tatiana <poluninat.m@gmail.com>
2021-04-01 16:29:41 +0300

    panel imgs size

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=f52b4ad"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=50164f2">
+          href="/css/main.css?v=18e0f35">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=c48947c"></script>
+    <script src="/js/main.js?v=4d50ad9"></script>
 
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=f52b4ad"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=50164f2">
-    <script src="/js/main.js?v=c48947c"></script>
+          href="/css/main.css?v=18e0f35">
+    <script src="/js/main.js?v=4d50ad9"></script>
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
 

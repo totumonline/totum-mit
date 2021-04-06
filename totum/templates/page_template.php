31305009813444be1a4bebb2c1fc8af6bbf4f71b
Tatiana <poluninat.m@gmail.com>
2021-04-06 14:04:04 +0300

    fx deleting in source treeView by button in panel

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=f52b4ad"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=995bd09">
+          href="/css/main.css?v=bfb9482">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=deddc46"></script>
+    <script src="/js/main.js?v=8f9928b"></script>
 
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=f52b4ad"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=995bd09">
-    <script src="/js/main.js?v=deddc46"></script>
+          href="/css/main.css?v=bfb9482">
+    <script src="/js/main.js?v=8f9928b"></script>
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
 

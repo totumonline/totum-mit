a696b66356c1535498c36d63f5c4b1a9a2fa5667
Tatiana <poluninat.m@gmail.com>
2021-04-14 11:09:32 +0300

    fx remove Add buttons in restoreView

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=feffa56"></script>
+    <script src="/js/main.js?v=fccc56c"></script>
 
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=5450711">
-    <script src="/js/main.js?v=feffa56"></script>
+    <script src="/js/main.js?v=fccc56c"></script>
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
 

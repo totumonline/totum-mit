ffdd321c6de0db97a335e773e52d01f3cdb634b7
Tatiana <poluninat.m@gmail.com>
2021-04-26 14:06:59 +0300

    front improvements

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=eb922b2"></script>
+    <script src="/js/main.js?v=8c0821f"></script>
 
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>

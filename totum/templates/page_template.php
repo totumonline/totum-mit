d12b4a0b112aa0a6dafc7af10ae5c9d3a5fc57d5
Tatiana <poluninat.m@gmail.com>
2021-05-20 15:50:44 +0300

    front - insertRow and panels

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=f52b4ad"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=fc3a608">
+          href="/css/main.css?v=fcb4758">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=771c059"></script>
+    <script src="/js/main.js?v=ffe30b9"></script>
 
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=f52b4ad"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=fc3a608">
-    <script src="/js/main.js?v=771c059"></script>
+          href="/css/main.css?v=fcb4758">
+    <script src="/js/main.js?v=ffe30b9"></script>
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
 

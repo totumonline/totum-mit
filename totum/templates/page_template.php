72c61a40b800ce59ebf4714cf27c903535db65ac
Tatiana <poluninat.m@gmail.com>
2021-05-26 16:26:39 +0300

    При постановке нескольких галочек и нажатии на панель надо первой открывать панель той строки на которой осуществлен клик #130

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=afc6532"></script>
+    <script src="/js/main.js?v=4657cfa"></script>
 
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>

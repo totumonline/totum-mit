aa933f473428fd177f1a8e433f53f1219abb92ee
Tatiana <poluninat.m@gmail.com>
2021-05-27 17:02:06 +0300

    fx front free selects

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=d88bbd2"></script>
+    <script src="/js/main.js?v=e63323c"></script>
 
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>

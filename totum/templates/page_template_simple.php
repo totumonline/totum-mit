2af2d15c2b1e10a798ccb0fbc003a32bc0d82f67
Tatiana <poluninat.m@gmail.com>
2021-07-27 14:25:58 +0300

    fx лог не записывается (или не выводится) если включены все галочки - изменена плашка с выбором типов логов.

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=62d8a04">
-    <script src="/js/main.js?v=5a12b52"></script>
+    <script src="/js/main.js?v=d484253"></script>
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
 

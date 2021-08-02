2dcb7d04d688b3f0640db4130878e06ef69ea1cb
Tatiana <poluninat.m@gmail.com>
2021-08-02 13:54:13 +0300

    fx деление на ноль в Математических операциях при пустой колонке

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=e1557e8"></script>
+    <script src="/js/main.js?v=7bf35ff"></script>
 
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=5e83ce0">
-    <script src="/js/main.js?v=e1557e8"></script>
+    <script src="/js/main.js?v=7bf35ff"></script>
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>
 

9c0513248359a7f10ee1cd1b0edf9582b30dee51
Tatiana <poluninat.m@gmail.com>
2021-08-04 15:37:17 +0300

    fxs front

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=f2bff67"></script>
+    <script src="/js/main.js?v=a077611"></script>
 
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>

082c6d91f69e226fcece4bef4750272c0e7219f5
Tatiana <poluninat.m@gmail.com>
2021-08-12 15:21:40 +0300

    bin/totum schemas-list; schemas-update --exclude

---
+++ totum/commands/SchemasList.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+
+namespace totum\commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use totum\config\Conf;
+
+class SchemasList extends Command
+{
+    protected function configure()
+    {
+        $this->setName('schemas-list')
+            ->setDescription('List of active schemas');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        foreach (array_unique(array_values(Conf::getSchemas())) as $schemaName) {
+            $output->writeln($schemaName);
+        }
+    }
+}
--- totum/commands/SchemasUpdates.php
+++ totum/commands/SchemasUpdates.php
@@ -21,20 +21,36 @@ class SchemasUpdates extends Command
                 'Enter source name',
                 'totum_' . (new Conf())->getLang()
             )
-            ->addArgument('file', InputOption::VALUE_REQUIRED, 'Enter schema update filepath', 'sys_update');
+            ->addArgument('file', InputOption::VALUE_REQUIRED, 'Enter schema update filepath', 'sys_update')
+            ->addOption('exclude', mode: InputOption::VALUE_IS_ARRAY|InputOption::VALUE_OPTIONAL, description: 'Enter schema names for exclude', default: []);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $matches=$input->getArgument('matches');
-        $file=$input->getArgument('file');
+        $matches = $input->getArgument('matches');
+        $file = $input->getArgument('file');
+
+        $excludeIn = $input->getOption('exclude') ?? [];
+        $exclude=[];
+        foreach ($excludeIn as $_){
+            foreach (preg_split('/\s*?[, ]\s*?/', $_) as $s){
+                if($s=trim($s)){
+                    $exclude[]=$s;
+                }
+            }
+        }
 
         foreach (array_unique(array_values(Conf::getSchemas())) as $schemaName) {
-            $output->writeln('update ' . $schemaName." with source $matches from $file");
+            if (in_array($schemaName, $exclude)) {
+                $output->writeln('EXCLUDE ' . $schemaName);
+                continue;
+            }
 
-            $p=popen("{$_SERVER['SCRIPT_FILENAME']} schema-update $matches $file -s $schemaName", 'r');
+            $output->writeln('update ' . $schemaName . " with source $matches from $file");
+
+            $p = popen("{$_SERVER['SCRIPT_FILENAME']} schema-update $matches $file -s $schemaName", 'r');
             while (is_resource($p) && $p && !feof($p)) {
-                $output->write("  ".fread($p, 1024));
+                $output->write("  " . fread($p, 1024));
             }
             pclose($p);
         }

f7729d334709783ecbdef54270356cbb6a68adfa
Tatiana <poluninat.m@gmail.com>
2021-08-23 14:07:47 +0300

    + — убрать подсветку красным при none если поле не подключено к строчн части + reload в мобильной версии при развороте

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=74f164a"></script>
+    <script src="/js/main.js?v=44376e5"></script>
 
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>

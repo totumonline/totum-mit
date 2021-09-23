880512c830b347d08e21f52d1f438e8f62d72c91
Tatiana <poluninat.m@gmail.com>
2021-09-23 11:51:42 +0300

    tableChanged fx

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=d406eee">
-    <script src="/js/main.js?v=465ffdd"></script>
+    <script src="/js/main.js?v=4875e09"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

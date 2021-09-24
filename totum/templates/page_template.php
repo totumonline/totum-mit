8df2e40a8d05aee2a1414a5ff5e233effc22d1d5
Tatiana <poluninat.m@gmail.com>
2021-09-24 13:45:34 +0300

    — окно поиска при свернутом дереве

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=d406eee">
+          href="/css/main.css?v=2664d2d">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=bc73b96"></script>
+    <script src="/js/main.js?v=eced981"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=d406eee">
-    <script src="/js/main.js?v=bc73b96"></script>
+          href="/css/main.css?v=2664d2d">
+    <script src="/js/main.js?v=eced981"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

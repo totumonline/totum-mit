701deeadc7c8b5c7f865329f2f7b0f72566ced46
Tatiana <poluninat.m@gmail.com>
2021-09-28 14:36:05 +0300

    focus in js-filter — когда строки появляются с обратным порядком они все равно в прямом

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=3f38959"></script>
+    <script src="/js/main.js?v=d6e71f3"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=2664d2d">
-    <script src="/js/main.js?v=3f38959"></script>
+    <script src="/js/main.js?v=d6e71f3"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

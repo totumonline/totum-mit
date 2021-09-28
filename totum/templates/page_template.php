55c9d6aa96d2fd49330603e4bf90beabab5c092d
Tatiana <poluninat.m@gmail.com>
2021-09-28 18:19:32 +0300

    на мобиле минимальный размер нотификаций

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=6b4e536">
+          href="/css/main.css?v=3eb9d7c">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=98c7486"></script>
+    <script src="/js/main.js?v=5103937"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

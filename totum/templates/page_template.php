d76ae6a06fd62ae6504d92de18beae8958002663
Tatiana <poluninat.m@gmail.com>
2021-09-29 15:59:55 +0300

    — отступ на штамп заголовка таблицы на мобиле убрать — базовый цвет заголовков строчной части whitesmoke — плейсхолдеры не показываются в панелях — глючно заполняется Панель селекта для изменения — постановка ; вне функции предлагает заполнить ее параметр — при быстром заполнении функции через / сделать альтернативу через ;

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=3eb9d7c">
+          href="/css/main.css?v=59ffab9">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=5103937"></script>
+    <script src="/js/main.js?v=160df2a"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=3eb9d7c">
-    <script src="/js/main.js?v=5103937"></script>
+          href="/css/main.css?v=59ffab9">
+    <script src="/js/main.js?v=160df2a"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

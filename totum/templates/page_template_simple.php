539a6c11836dda8b84140ef714da13fefba9fba7
Tatiana <poluninat.m@gmail.com>
2021-10-04 19:22:55 +0300

    table edit buttons

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -9,7 +9,7 @@
           type="text/css"
           href="/css/main.css?v=59ffab9">
     <script src="/js/main.js?v=0575dfe"></script>
-    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
+    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>

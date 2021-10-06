595ce148fdf38dd24e35f8cb7bd384fa2063ea1c
Tatiana <poluninat.m@gmail.com>
2021-10-06 15:45:46 +0300

    fx front

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=40fc3bb">
-    <script src="/js/main.js?v=0094a4d"></script>
+          href="/css/main.css?v=212e4ee">
+    <script src="/js/main.js?v=339bad3"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

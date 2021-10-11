59b729032877de5a878342c0619396c55ebcdd95
Tatiana <poluninat.m@gmail.com>
2021-10-11 14:13:37 +0300

    .n.just-inserted bg-color

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=331278c">
+          href="/css/main.css?v=2f0d20f">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=6143e44"></script>
+    <script src="/js/main.js?v=bb1cf13"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=331278c">
-    <script src="/js/main.js?v=6143e44"></script>
+          href="/css/main.css?v=2f0d20f">
+    <script src="/js/main.js?v=bb1cf13"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

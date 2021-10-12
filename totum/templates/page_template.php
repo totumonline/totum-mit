89ff04b0075f87dbe3fca8ad74c2b8b7f2ded09c
Tatiana <poluninat.m@gmail.com>
2021-10-12 17:29:04 +0300

    fx float blocks problems

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=4551313"></script>
+    <script src="/js/main.js?v=0757854"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

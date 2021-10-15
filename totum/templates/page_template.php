c2b58de6cb6ef4518bc8cebcc3bdeace8d3a17cc
Tatiana <poluninat.m@gmail.com>
2021-10-15 14:39:33 +0300

    background for edit-in-form

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=118eed5"></script>
+    <script src="/js/main.js?v=128788e"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

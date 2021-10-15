5d8b713c1f1300b5cb3edc1248712b7a2390cfbd
Tatiana <poluninat.m@gmail.com>
2021-10-15 12:58:07 +0300

    footer-buttons .fa-lock

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=c048b97">
+          href="/css/main.css?v=ae4fbad">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=4fca1bf"></script>
+    <script src="/js/main.js?v=542d9e1"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=c048b97">
-    <script src="/js/main.js?v=4fca1bf"></script>
+          href="/css/main.css?v=ae4fbad">
+    <script src="/js/main.js?v=542d9e1"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

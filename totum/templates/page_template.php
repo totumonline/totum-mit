93659f4753841cfe03a21ae6d1cb9eccc3362464
Tatiana <poluninat.m@gmail.com>
2021-10-20 17:08:24 +0300

    LANG: eng -> en

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=4648425"></script>
+    <script src="/js/main.js?v=01d13f2"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

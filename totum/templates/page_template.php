7f5c8b8ec255e2cf432a63b9ebb75e2e615ce29d
Tatiana <poluninat.m@gmail.com>
2021-10-22 18:16:16 +0300

    totum-code btn-with-lock

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=5db17ad">
+          href="/css/main.css?v=35a1d4d">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=9efef38"></script>
+    <script src="/js/main.js?v=a12cb38"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

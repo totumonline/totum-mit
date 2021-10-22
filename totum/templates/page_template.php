a4931f1fc14620fbbd5001439c0acde25d1e40e0
Tatiana <poluninat.m@gmail.com>
2021-10-22 15:17:41 +0300

    treeHideAddButton

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=6e1c689"></script>
+    <script src="/js/main.js?v=eb4f467"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=c19d343">
-    <script src="/js/main.js?v=6e1c689"></script>
+    <script src="/js/main.js?v=eb4f467"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

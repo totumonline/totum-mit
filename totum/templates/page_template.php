fcf5400153599e542ec55dc7447fa0ac6bf31484
Tatiana <poluninat.m@gmail.com>
2021-10-22 12:59:20 +0300

    field.multiSeparator & fx FF borders

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=a9bf7ac">
+          href="/css/main.css?v=c19d343">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=cf4177d"></script>
+    <script src="/js/main.js?v=6e1c689"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=a9bf7ac">
-    <script src="/js/main.js?v=cf4177d"></script>
+          href="/css/main.css?v=c19d343">
+    <script src="/js/main.js?v=6e1c689"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

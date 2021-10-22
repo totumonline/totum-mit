16e76d69d5cfdcba537f2e9a877815e715945fc8
Tatiana <poluninat.m@gmail.com>
2021-10-22 15:48:21 +0300

    FocusIt delete

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=76e5586"></script>
+    <script src="/js/main.js?v=9efef38"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=5db17ad">
-    <script src="/js/main.js?v=76e5586"></script>
+    <script src="/js/main.js?v=9efef38"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

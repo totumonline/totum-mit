94613c1f530e294c676d0adeed9fe9d39b07291c
Tatiana <poluninat.m@gmail.com>
2021-10-25 14:40:15 +0300

    some front fixes

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=35a1d4d">
-    <script src="/js/main.js?v=5f7c80f"></script>
+          href="/css/main.css?v=3a84905">
+    <script src="/js/main.js?v=7105315"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

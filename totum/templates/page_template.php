201fb802204df67f4d450ad9541b8abaef1fc851
Tatiana <poluninat.m@gmail.com>
2021-10-25 18:31:30 +0300

    CodeMirror-readonly hide cursor

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=3a84905">
+          href="/css/main.css?v=d14ccae">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=a01f32b"></script>
+    <script src="/js/main.js?v=77e433c"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=3a84905">
-    <script src="/js/main.js?v=a01f32b"></script>
+          href="/css/main.css?v=d14ccae">
+    <script src="/js/main.js?v=77e433c"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

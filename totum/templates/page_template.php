6f77276117a0a312d6d86e694ad5458a05ac8314
Tatiana <poluninat.m@gmail.com>
2021-10-25 15:50:36 +0300

    подсвечивать красным функции без названий параметров

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=7105315"></script>
+    <script src="/js/main.js?v=b635873"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

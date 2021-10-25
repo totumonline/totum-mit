53c6d86c3aa9f00ce824045553e6c2d527ed8096
Tatiana <poluninat.m@gmail.com>
2021-10-25 18:34:20 +0300

    fx postfix without **

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=77e433c"></script>
+    <script src="/js/main.js?v=5e3e65c"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

5b832dd8079642f7cfb1fbaff7b0272f083a03d5
Tatiana <poluninat.m@gmail.com>
2021-10-25 18:07:35 +0300

    fx error notification in insert row, page size limitation

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=3a84905">
-    <script src="/js/main.js?v=b635873"></script>
+    <script src="/js/main.js?v=a01f32b"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

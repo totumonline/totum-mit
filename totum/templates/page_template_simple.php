4b7f882e343e1df56a639777e0c51a36bd3f4e13
Tatiana <poluninat.m@gmail.com>
2021-10-27 13:08:52 +0300

    fx colorizer

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=d14ccae">
-    <script src="/js/main.js?v=5e3e65c"></script>
+    <script src="/js/main.js?v=9feee4e"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

1a5229e28683fed87e6033a93f12a68f3ffd0de6
Tatiana <poluninat.m@gmail.com>
2021-10-27 14:03:48 +0300

    editPanel imprvs

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=d14ccae">
+          href="/css/main.css?v=f27b5b9">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=9feee4e"></script>
+    <script src="/js/main.js?v=0bd3b91"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

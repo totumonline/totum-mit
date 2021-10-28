2c3af0d73d50b6cc862781e010318af1c9d765d4
Tatiana <poluninat.m@gmail.com>
2021-10-28 14:28:42 +0300

    fx datetime resizing

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=69e1451">
+          href="/css/main.css?v=ec44eb7">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=9d56237"></script>
+    <script src="/js/main.js?v=d7a6f73"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

de595d3433286482d5d874137676a8023292f653
Tatiana <poluninat.m@gmail.com>
2021-11-01 14:21:33 +0300

    fx add to log in adding row field value

--- totum/tableTypes/RealTables.php
+++ totum/tableTypes/RealTables.php
@@ -1257,7 +1257,7 @@ abstract class RealTables extends aTable
                 )) {
                 $_channel = 'inner';
                 $newVal = $this->insertRowSetData[$v['name']];
-                unset($this->insertRowSetData[$v['name']]);
+
             }
 
             $changedData[$v['name']] = $field->add(
@@ -1302,11 +1302,11 @@ abstract class RealTables extends aTable
                 foreach ($changedData as $k => $v) {
                     if (key_exists($k, $this->fields)) {
                         $Field = Field::init($this->fields[$k], $this);
-                        $this->addToALogAdd($Field, $channel, $this->tbl, $row, $addData);
+                        $this->addToALogAdd($Field, $channel, $this->tbl, $row, $this->insertRowSetData ?? $addData);
                     }
                 }
                 /******aLog*****/
-
+                $this->insertRowSetData = null;
                 return $row;
             }
         } else {

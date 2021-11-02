87fcec35a69126daba6617e0a8f929986b0318eb
Tatiana <poluninat.m@gmail.com>
2021-11-02 15:38:06 +0300

    сделать кнопку скрытия админ-интерфейса доступной из страниц дерева

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=252f2ba"></script>
+    <script src="/js/main.js?v=5120207"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

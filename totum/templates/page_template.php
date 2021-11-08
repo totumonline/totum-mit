eca13a112510134c74be90052b679bbe3174fc3f
Tatiana <poluninat.m@gmail.com>
2021-11-08 19:17:51 +0300

    panel editing code by link

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=40699b8"></script>
+    <script src="/js/main.js?v=e846c19"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

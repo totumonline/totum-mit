1b001dc81e32baccf3a50a25dadad3d452b5646d
Tatiana <poluninat.m@gmail.com>
2021-11-10 15:05:40 +0300

    fx group checkbox editing

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=b7d0bba"></script>
+    <script src="/js/main.js?v=d6bf0a8"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

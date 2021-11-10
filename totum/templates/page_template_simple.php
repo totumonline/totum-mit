69ae92d3c8dcdb148eb346481b47de44cc087329
Tatiana <poluninat.m@gmail.com>
2021-11-10 18:22:34 +0300

    panels view title css

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=6ffb858">
-    <script src="/js/main.js?v=83b1128"></script>
+          href="/css/main.css?v=a223a42">
+    <script src="/js/main.js?v=770a4b2"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

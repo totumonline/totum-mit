576e2414c72d627d6659384da9b991cbdff195f2
Tatiana <poluninat.m@gmail.com>
2021-11-10 16:25:15 +0300

    panels view title css

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=33f8bd4">
+          href="/css/main.css?v=6ffb858">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=33bc542"></script>
+    <script src="/js/main.js?v=83b1128"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

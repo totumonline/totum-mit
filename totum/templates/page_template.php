dc2f81f5e303e5fb520d58c2fb4c0819b190fe4f
Tatiana <poluninat.m@gmail.com>
2021-11-11 15:41:04 +0300

    попытка открыть на изменение после добавления без сохранения селекта

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=9c39f85"></script>
+    <script src="/js/main.js?v=0e039fe"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

f550e93fdb7a41f96f2c18850b35001c58dcc22f
Tatiana <poluninat.m@gmail.com>
2021-11-11 16:24:28 +0300

    доработать перевод фокуса в строке добавления

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=0e039fe"></script>
+    <script src="/js/main.js?v=67deafe"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=5cb4699">
-    <script src="/js/main.js?v=0e039fe"></script>
+    <script src="/js/main.js?v=67deafe"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

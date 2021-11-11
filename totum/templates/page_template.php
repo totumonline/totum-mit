c2ee4ab7852feb00fcd87fa21971ee456bce3109
Tatiana <poluninat.m@gmail.com>
2021-11-11 16:39:17 +0300

    кнопка откл админского инт показывается при перекл в прост пользователя

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=879e292"></script>
+    <script src="/js/main.js?v=1dcfd03"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=4f57494">
-    <script src="/js/main.js?v=879e292"></script>
+    <script src="/js/main.js?v=1dcfd03"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

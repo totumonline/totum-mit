19a86b89b9bd7d5cbc5bed60a0dab0a317adbacc
Tatiana <poluninat.m@gmail.com>
2021-11-15 18:47:16 +0300

    fx checkboxes

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=232d4ad">
-    <script src="/js/main.js?v=394ffda"></script>
+          href="/css/main.css?v=f01779a">
+    <script src="/js/main.js?v=09a5685"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

98987535ec9439beebfc701e9608189471d1a722
Tatiana <poluninat.m@gmail.com>
2021-11-15 16:14:21 +0300

    показывать по другому sort не работает в панели в панели для расчета селектов важен порядок sort как при расчете кода

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=232d4ad">
-    <script src="/js/main.js?v=fbf70c4"></script>
+    <script src="/js/main.js?v=c9d16ec"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

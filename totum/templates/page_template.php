8e6b2b2198cbdb5a1d99d9e7ccab07e47bcef8e1
Tatiana <poluninat.m@gmail.com>
2021-11-15 15:15:41 +0300

    в пэнел вью поломалась правая кнопка мыши клик в кнопку в panel-вью не должен вызывать выделения поля

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=7261cc6"></script>
+    <script src="/js/main.js?v=fbf70c4"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=232d4ad">
-    <script src="/js/main.js?v=7261cc6"></script>
+    <script src="/js/main.js?v=fbf70c4"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

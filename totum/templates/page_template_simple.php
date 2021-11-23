0e4433cd70ce168e37bf1b5b331e406daefb4573
Tatiana <poluninat.m@gmail.com>
2021-11-23 17:05:38 +0300

    view improves

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=2805362">
-    <script src="/js/main.js?v=725fe50"></script>
+          href="/css/main.css?v=a74ae43">
+    <script src="/js/main.js?v=507b6b9"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

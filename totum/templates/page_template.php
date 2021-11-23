94f570eef39b13d16b11d74fa5947f554a735d2d
Tatiana <poluninat.m@gmail.com>
2021-11-23 16:49:02 +0300

    panels view improves

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=1f5345e">
+          href="/css/main.css?v=2805362">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=bd31313"></script>
+    <script src="/js/main.js?v=725fe50"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

d6287a02c025171cb3d02d6bda4389014771d6dd
Tatiana <poluninat.m@gmail.com>
2021-11-25 18:34:13 +0300

    ошибка в заполеннии / если есть параметры

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=84d5999"></script>
+    <script src="/js/main.js?v=969fec4"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

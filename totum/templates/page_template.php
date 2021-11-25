3242141d2651b020e3aefd14a10f5927d4e42335
Tatiana <poluninat.m@gmail.com>
2021-11-25 16:44:08 +0300

    fx contexpanel

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=f960c5f"></script>
+    <script src="/js/main.js?v=35bc3a5"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

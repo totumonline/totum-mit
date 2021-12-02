d976a72c5cbc0893116dffd93d48e6b2bc844729
Tatiana <poluninat.m@gmail.com>
2021-12-02 18:15:52 +0300

    burger for mobile table-buttons

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=1f8d1b0">
+          href="/css/main.css?v=13f9ddf">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=220a0fd"></script>
+    <script src="/js/main.js?v=9029fb4"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?6"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=1f8d1b0">
-    <script src="/js/main.js?v=220a0fd"></script>
+          href="/css/main.css?v=13f9ddf">
+    <script src="/js/main.js?v=9029fb4"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?6"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

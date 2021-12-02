1ae6be0f831e913fd5f229a293cd5cba58e660fd
Tatiana <poluninat.m@gmail.com>
2021-12-02 18:54:55 +0300

    burger for mobile table-buttons

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=0160cfa"></script>
+    <script src="/js/main.js?v=b6327bf"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?6"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=f21cde6">
-    <script src="/js/main.js?v=0160cfa"></script>
+    <script src="/js/main.js?v=b6327bf"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?6"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

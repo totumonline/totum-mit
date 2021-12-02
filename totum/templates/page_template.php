e731002a02ca4d871ee5bcb3943b6c0af17f3691
Tatiana <poluninat.m@gmail.com>
2021-12-02 16:39:20 +0300

    mobile resize reload button

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=c9d5a2c">
+          href="/css/main.css?v=8e42cc0">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=d3c7372"></script>
+    <script src="/js/main.js?v=e3330f9"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?6"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=c9d5a2c">
-    <script src="/js/main.js?v=d3c7372"></script>
+          href="/css/main.css?v=8e42cc0">
+    <script src="/js/main.js?v=e3330f9"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?6"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

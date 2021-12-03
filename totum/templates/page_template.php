c1b37f0b476dfa9d790b57c62460202f16683157
Tatiana <poluninat.m@gmail.com>
2021-12-03 16:51:21 +0300

    front mobile improves

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=3c0df33">
+          href="/css/main.css?v=e3dc0cb">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=e15c391"></script>
+    <script src="/js/main.js?v=453f8c1"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?6"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

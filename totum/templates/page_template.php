f61276d62e96a0193026c4d2ed35edb1c98b9ac8
Tatiana <poluninat.m@gmail.com>
2021-12-03 14:16:07 +0300

    header panels recolor; schemas update

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=294db2a">
+          href="/css/main.css?v=e110927">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=665d99b"></script>
+    <script src="/js/main.js?v=22c2d3d"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?6"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=294db2a">
-    <script src="/js/main.js?v=665d99b"></script>
+          href="/css/main.css?v=e110927">
+    <script src="/js/main.js?v=22c2d3d"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?6"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

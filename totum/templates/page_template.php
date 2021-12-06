cb086209722da2a2554a419f2f192e976fe9eeb8
Tatiana <poluninat.m@gmail.com>
2021-12-06 14:44:34 +0300

    front  improves

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=86c5e9f"></script>
+    <script src="/js/main.js?v=5604742"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?6"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=031609d">
-    <script src="/js/main.js?v=86c5e9f"></script>
+    <script src="/js/main.js?v=5604742"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?6"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

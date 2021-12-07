6d267721462084fedeb5f9410ccedfacfa35401b
Tatiana <poluninat.m@gmail.com>
2021-12-07 16:12:22 +0300

    images border-radius

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=42fd1bb">
+          href="/css/main.css?v=c9fad87">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=b5d6710"></script>
+    <script src="/js/main.js?v=c769d97"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?6"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

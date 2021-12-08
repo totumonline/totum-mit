40c14472e6a9785f8ff849dfc4ef12c13d626644
Tatiana <poluninat.m@gmail.com>
2021-12-08 14:30:49 +0300

    filters td/th css

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=aed88dc">
+          href="/css/main.css?v=7ff8d36">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=dd1374b"></script>
+    <script src="/js/main.js?v=ac89884"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?6"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

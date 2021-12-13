0fbdf81215506c1885d7e3e800e87c6e7996d057
Tatiana <poluninat.m@gmail.com>
2021-12-13 19:17:04 +0300

    max-width for selectPabelBit with img a

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=496c085">
+          href="/css/main.css?v=031fc74">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=d1c0e7a"></script>
+    <script src="/js/main.js?v=3cfd0ba"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?6"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

7e843796ba8da18b9a5cb8e76ba846fb53aa4569
Tatiana <poluninat.m@gmail.com>
2021-12-14 14:45:07 +0300

    fx front errors

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=031fc74">
-    <script src="/js/main.js?v=3cfd0ba"></script>
+    <script src="/js/main.js?v=d739e92"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?6"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

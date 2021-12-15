4b4c3aba4474a45367ce768dd2cd3cd1f2974592
Tatiana <poluninat.m@gmail.com>
2021-12-15 16:26:04 +0300

    linktoedit

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=39c1ac2"></script>
+    <script src="/js/main.js?v=4404acd"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?7"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

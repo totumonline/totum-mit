5e8eb1ae0f5edfd4d6d19235c9393a1f0a3a4249
Tatiana <poluninat.m@gmail.com>
2021-12-15 16:15:04 +0300

    linktoedit

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=245c62b"></script>
+    <script src="/js/main.js?v=fe996d5"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?7"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=031fc74">
-    <script src="/js/main.js?v=245c62b"></script>
+    <script src="/js/main.js?v=fe996d5"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?7"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

0966d18bf3c8e80c08f60be235af0d3fb5da4c72
Tatiana <poluninat.m@gmail.com>
2021-12-16 13:14:27 +0300

    fx insert focuses

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=904b60f"></script>
+    <script src="/js/main.js?v=875ac7c"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?7"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

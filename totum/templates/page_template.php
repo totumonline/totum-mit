235a60f12b016fc5208b6c3bb4aeef3acdfb9dfe
Tatiana <poluninat.m@gmail.com>
2021-12-21 16:23:26 +0300

    -- ошибки в вызове code по @ и "name"

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=bb3efa7">
+          href="/css/main.css?v=35d64cf">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=97e8da9"></script>
+    <script src="/js/main.js?v=c6229a3"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?8"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=bb3efa7">
-    <script src="/js/main.js?v=97e8da9"></script>
+          href="/css/main.css?v=35d64cf">
+    <script src="/js/main.js?v=c6229a3"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?8"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

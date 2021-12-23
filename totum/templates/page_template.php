4cd939492d9f2ae2e2326c87316d5e03fcc5eff7
Tatiana <poluninat.m@gmail.com>
2021-12-23 17:07:01 +0300

    "tables" field param

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=766d558"></script>
+    <script src="/js/main.js?v=e725996"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?8"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=25e91cf">
-    <script src="/js/main.js?v=766d558"></script>
+    <script src="/js/main.js?v=e725996"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?8"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

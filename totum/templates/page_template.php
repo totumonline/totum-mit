a8e658fc7b177baa0a45c1bc7e2ff16f87bd100a
Tatiana <poluninat.m@gmail.com>
2021-12-30 17:18:33 +0300

    fx front: locker & colorizer

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=f07adcf"></script>
+    <script src="/js/main.js?v=eab8500"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?8"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=7fef64d">
-    <script src="/js/main.js?v=f07adcf"></script>
+    <script src="/js/main.js?v=eab8500"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?8"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

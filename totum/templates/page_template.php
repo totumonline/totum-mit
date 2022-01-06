4f1d3cc68c8a988ccdd7163c0ef41d1f4f3394e6
Tatiana <poluninat.m@gmail.com>
2022-01-06 16:23:39 +0300

    не работает поиск по дереву изнутри цикла

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=229fae2"></script>
+    <script src="/js/main.js?v=5257e67"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?8"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

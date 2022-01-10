0062abfc0ac118a5f9ef36e3425ccdfc0443f0cd
Tatiana <poluninat.m@gmail.com>
2022-01-10 17:05:38 +0300

    edit last comment

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,8 +18,8 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=981a5ab"></script>
-    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?9"></script>
+    <script src="/js/main.js?v=9562ccf"></script>
+    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?10"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,8 +8,8 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=065c85e">
-    <script src="/js/main.js?v=981a5ab"></script>
-    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?9"></script>
+    <script src="/js/main.js?v=9562ccf"></script>
+    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?10"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>

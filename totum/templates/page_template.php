14dd321e589c9ea0cf314eecd1106bfafe17885d
Tatiana <poluninat.m@gmail.com>
2022-01-10 17:24:22 +0300

    rowcomment

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=199a190">
+          href="/css/main.css?v=92a8519">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=b164779"></script>
+    <script src="/js/main.js?v=71ed571"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?10"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=199a190">
-    <script src="/js/main.js?v=b164779"></script>
+          href="/css/main.css?v=92a8519">
+    <script src="/js/main.js?v=71ed571"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?10"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

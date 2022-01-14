0d25066c5eed887f2e7db3ec5c7e62ac75d47f08
Tatiana <poluninat.m@gmail.com>
2022-01-14 19:20:30 +0300

    fx front hidden fields error

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=1632f77"></script>
+    <script src="/js/main.js?v=358bb26"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?12"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

c242900c20ad284ad019671fa7348e79c228f942
Tatiana <poluninat.m@gmail.com>
2022-01-17 14:18:08 +0300

    fx front - deselect value in single tree

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=f2ac475"></script>
+    <script src="/js/main.js?v=7368fea"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?12"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

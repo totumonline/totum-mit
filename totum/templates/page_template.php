0f3e4accd377c571c3178957e340caaad66d7fc4
Tatiana <poluninat.m@gmail.com>
2022-01-17 16:58:13 +0300

    fx select focus in insertRow

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=5eabed8"></script>
+    <script src="/js/main.js?v=426d1dc"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?12"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

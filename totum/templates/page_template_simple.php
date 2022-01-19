e890b418bfc716a3ea62c3ef0e54460da6d98766
Tatiana <poluninat.m@gmail.com>
2022-01-19 17:30:00 +0300

    fx при попытке дублировать поле в расчетную в цикле говорит Таблица не найдена

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=de287ec">
-    <script src="/js/main.js?v=e5f7a69"></script>
+    <script src="/js/main.js?v=11a0cb1"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?12"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

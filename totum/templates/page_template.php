9fd8059c8339eeb9fa8b4f011f00102383d768d0
Tatiana <poluninat.m@gmail.com>
2022-01-20 18:36:53 +0300

    fx: codeOnClick in insertPanel

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=1c01728"></script>
+    <script src="/js/main.js?v=427176b"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?12"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

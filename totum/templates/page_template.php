1f41714d5afdd4607e0409344f23ea4a97290858
Tatiana <poluninat.m@gmail.com>
2022-01-24 16:45:17 +0300

    kanban_html css

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=d592592">
+          href="/css/main.css?v=09add9e">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,8 +18,8 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=a7cdebb"></script>
-    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?12"></script>
+    <script src="/js/main.js?v=8611fc2"></script>
+    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 
 

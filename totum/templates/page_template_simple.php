58bda70a9695afd8206037cd3d49b154fc336ef5
Tatiana <poluninat.m@gmail.com>
2022-01-24 15:53:32 +0300

    kanban_html css

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=261df07">
-    <script src="/js/main.js?v=3d20fe8"></script>
+          href="/css/main.css?v=d592592">
+    <script src="/js/main.js?v=a7cdebb"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?12"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

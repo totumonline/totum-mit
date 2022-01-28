1f9e05dcbc90ce0e25a4a490f0ccb496b27f1f19
Tatiana <poluninat.m@gmail.com>
2022-01-28 18:26:52 +0300

    treeView n-sorting

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=29a3f1c"></script>
+    <script src="/js/main.js?v=6ae27d1"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

c2e0c6caeda5c7c044f220f502916f56c9deb376
Tatiana <poluninat.m@gmail.com>
2022-01-28 13:54:55 +0300

    treeView n-sorting

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -11,14 +11,14 @@
 
     <?php
     if ($isCreatorView ?? null) { ?>
-        <script src="/js/functions.js?v=a3b9a58!"></script>
+        <script src="/js/functions.js?v=08d9dc3"></script>
         <?php
         echo '<script>App.functions=App.functions.concat(' . $this->Config->getExtFunctionsTemplates() . ')</script>';
         ?>
         <?php
     } ?>
 
-    <script src="/js/main.js?v=55584d5"></script>
+    <script src="/js/main.js?v=85f8cd7"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

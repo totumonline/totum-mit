e99eb14d83f2080884b151d1f0896cd7d3e4f802
Tatiana <poluninat.m@gmail.com>
2022-02-01 14:34:23 +0300

    print rotated table

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=7c6a2a4">
-    <script src="/js/main.js?v=7919874"></script>
+    <script src="/js/main.js?v=f41deea"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

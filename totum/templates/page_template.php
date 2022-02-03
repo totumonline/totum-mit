da5ec94da8f848b1497cd596e1b647242d98b702
Tatiana <poluninat.m@gmail.com>
2022-02-03 13:24:37 +0300

    interlace

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=1b1265c"></script>
+    <script src="/js/main.js?v=70d06b5"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

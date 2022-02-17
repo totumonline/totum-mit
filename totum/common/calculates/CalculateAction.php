369923561c153e170dccbc2166bb99d80c732754
Tatiana <poluninat.m@gmail.com>
2022-02-17 13:23:43 +0300

    fx error in tabs in cycles

--- totum/common/calculates/CalculateAction.php
+++ totum/common/calculates/CalculateAction.php
@@ -92,8 +92,8 @@ class CalculateAction extends Calculate
 
                     $schema = '';
 
-                    if(method_exists($this->Table->getTotum()->getConfig(), 'setHostSchema')){
-                        $schema = '--schema "'.$this->Table->getTotum()->getConfig()->getSchema().'"';
+                    if (method_exists($this->Table->getTotum()->getConfig(), 'setHostSchema')) {
+                        $schema = '--schema "' . $this->Table->getTotum()->getConfig()->getSchema() . '"';
                     }
 
                     return `cd {$path} && bin/totum exec {$schema} {$this->Table->getUser()->getId()} {$data} {$test}`;
@@ -999,7 +999,7 @@ class CalculateAction extends Calculate
 
         if ($this->Table->getTableRow()['type'] === 'cycles' && str_starts_with($this->varName,
                 'tab_') && !empty($this->row['id']) && $tableDestRow['type'] != 'calcs') {
-
+            $params['cycle'] = $params['cycle'] ?? null;
             $link .= $this->Table->getTableRow()['top'] . '/' . $this->Table->getTableRow()['id'] . '/' . ($params['cycle'] ?: $this->row['id']) . '/' . $tableDestRow['id'];
             $linkedTable = $this->Table->getTotum()->getTable($tableDestRow);
             $q_params['b'] = $this->varName;

b5bbfd3f838c76c7b3633ae95b243c514b066916
Tatiana <poluninat.m@gmail.com>
2022-04-07 15:28:57 +0300

    fx insert row

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=58507de"></script>
+    <script src="/js/main.js?v=faa43d7"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=f836198">
-    <script src="/js/main.js?v=58507de"></script>
+    <script src="/js/main.js?v=faa43d7"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

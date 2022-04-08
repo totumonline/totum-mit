3ae4b8b56b81b869c0d2b920a8cab08b517dc356
Tatiana <poluninat.m@gmail.com>
2022-04-08 15:19:26 +0300

    linktoinputselect codeselect as required

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -11,14 +11,14 @@
 
     <?php
     if ($isCreatorView ?? null) { ?>
-        <script src="/js/functions.js?v=28ec426"></script>
+        <script src="/js/functions.js?v=4085aac"></script>
         <?php
         echo '<script>App.functions=App.functions.concat(' . $this->Config->getExtFunctionsTemplates() . ')</script>';
         ?>
         <?php
     } ?>
 
-    <script src="/js/main.js?v=e111302"></script>
+    <script src="/js/main.js?v=7ecaad8"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

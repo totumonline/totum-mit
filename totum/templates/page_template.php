d04c483e406f082341f6ba824892191abb3d2dee
Tatiana <poluninat.m@gmail.com>
2022-04-08 16:06:38 +0300

    fx error in files Adding Form

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=7ecaad8"></script>
+    <script src="/js/main.js?v=086855c"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

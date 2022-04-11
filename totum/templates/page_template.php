76182c6c2d716b838a639e63582d3265fb0e40de
Tatiana <poluninat.m@gmail.com>
2022-04-11 16:53:53 +0300

    что-то дообновляется при перетаскивании панелей в канбан

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=c9f1cfd"></script>
+    <script src="/js/main.js?v=5db6494"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang=App.langs["<?= $this->Config->getLang() ?>"]</script>
 

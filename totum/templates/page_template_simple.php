2b50f0f87621c7c8f1e889f4217cb3278b223360
Tatiana <poluninat.m@gmail.com>
2022-04-13 15:32:21 +0300

    "," in number web input format; fx linktoedit string/number; длинные заголовки iframe съезжают; decreasing print timeout

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=5889b5d">
-    <script src="/js/main.js?v=2296192"></script>
+          href="/css/main.css?v=277e8f5">
+    <script src="/js/main.js?v=e7e32bf"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

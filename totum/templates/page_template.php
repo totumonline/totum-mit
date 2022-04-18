23c68a7e461ecc213b8897307662a390960628d3
Tatiana <poluninat.m@gmail.com>
2022-04-18 16:42:28 +0300

    fx front source-table-edit in row and panel

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=87ff876"></script>
+    <script src="/js/main.js?v=05b5c33"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

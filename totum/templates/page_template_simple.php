8d073b4d50ab02f0ff99842c76f9d9196cc9054c
Tatiana <poluninat.m@gmail.com>
2022-04-18 17:16:34 +0300

    en loading.png for rows part

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=277e8f5">
-    <script src="/js/main.js?v=05b5c33"></script>
+    <script src="/js/main.js?v=f8cb71e"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

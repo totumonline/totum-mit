c0d947d59ee661bb42ace86b40b4509af3751c0c
Tatiana <poluninat.m@gmail.com>
2022-04-27 13:31:57 +0300

    block installing in public schema

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=64661ac"></script>
+    <script src="/js/main.js?v=2d41dd5"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?15"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=5ce5924">
-    <script src="/js/main.js?v=64661ac"></script>
+    <script src="/js/main.js?v=2d41dd5"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?15"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

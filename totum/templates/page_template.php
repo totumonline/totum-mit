2aa1e1a8397eadfa41f2edc3198d79ed5fa49480
Tatiana <poluninat.m@gmail.com>
2022-04-27 14:15:29 +0300

    $#rows - autocomplete

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,8 +18,8 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=2d41dd5"></script>
-    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?15"></script>
+    <script src="/js/main.js?v=ceaba86"></script>
+    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?16"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,8 +8,8 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=5ce5924">
-    <script src="/js/main.js?v=2d41dd5"></script>
-    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?15"></script>
+    <script src="/js/main.js?v=ceaba86"></script>
+    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?16"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>

214b1a16d08fe891d15f0a30f63487fad0ec2c02
Tatiana <poluninat.m@gmail.com>
2022-05-04 16:05:07 +0300

    js sorting with testasvalue

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=e348e90"></script>
+    <script src="/js/main.js?v=82a1ec4"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?16"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=5ce5924">
-    <script src="/js/main.js?v=e348e90"></script>
+    <script src="/js/main.js?v=82a1ec4"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?16"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

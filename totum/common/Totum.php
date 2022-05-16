bfc9b3313bb66104947b23fb704a88820217b30e
Tatiana <poluninat.m@gmail.com>
2022-05-16 14:33:53 +0300

    2.4.42.2

--- totum/common/Totum.php
+++ totum/common/Totum.php
@@ -27,7 +27,7 @@ use totum\tableTypes\tmpTable;
  */
 class Totum
 {
-    public const VERSION = '2.4.42.1';
+    public const VERSION = '2.4.42.2';
 
 
     public const TABLE_CODE_PARAMS = ['row_format', 'table_format', 'on_duplicate', 'default_action'];

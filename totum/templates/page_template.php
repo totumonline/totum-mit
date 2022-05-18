4661d8a3e79471676171c91979c080a9b94e6d81
Tatiana <poluninat.m@gmail.com>
2022-05-18 13:11:40 +0300

    changes in extended search

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=9e7029f"></script>
+    <script src="/js/main.js?v=de912e2"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?17"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=ede1f81">
-    <script src="/js/main.js?v=9e7029f"></script>
+    <script src="/js/main.js?v=de912e2"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?17"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

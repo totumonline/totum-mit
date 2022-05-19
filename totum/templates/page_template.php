dfb0792765fa4ab994a1586d676d4e5cf9fc97ca
Tatiana <poluninat.m@gmail.com>
2022-05-19 16:44:34 +0300

    fx повторно не открывается link...json в панели

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=57b3c26"></script>
+    <script src="/js/main.js?v=44831d4"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?17"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

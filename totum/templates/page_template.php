84ab8dce2f46e75ece4c473d3f69f6c6230a3b93
Tatiana <poluninat.m@gmail.com>
2022-05-19 14:32:34 +0300

    fx panelView table with restricted columns in mobile

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=91f4346"></script>
+    <script src="/js/main.js?v=25b494b"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?17"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

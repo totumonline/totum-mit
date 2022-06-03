6a0d65f2ba7a511b5db6765997f5a15f5c913903
Tatiana <poluninat.m@gmail.com>
2022-06-03 17:30:33 +0300

    icon for panel button

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=4e7c9ad">
+          href="/css/main.css?v=21b5cde">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=6596a07"></script>
+    <script src="/js/main.js?v=67e69cf"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?17"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

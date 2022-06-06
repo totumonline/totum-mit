bcd14068f14cc3b46f525194d4ebbbc58006e85d
Tatiana <poluninat.m@gmail.com>
2022-06-06 16:01:45 +0300

    rowButtons in panelview

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=21b5cde">
-    <script src="/js/main.js?v=058749e"></script>
+    <script src="/js/main.js?v=fdbc46a"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?17"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

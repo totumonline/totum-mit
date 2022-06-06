5183a1a1a623aeb6e0229d4d12130e5b6c94c8f0
Tatiana <poluninat.m@gmail.com>
2022-06-06 13:30:29 +0300

    pluses in trees

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=21b5cde">
-    <script src="/js/main.js?v=4d912d6"></script>
+    <script src="/js/main.js?v=a880f3c"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?17"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

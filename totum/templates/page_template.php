4bf7261378a31d1712bf157657cd42598a0d3d00
Tatiana <poluninat.m@gmail.com>
2022-07-05 17:37:14 +0300

    rm title from setTableFormat

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -11,14 +11,14 @@
 
     <?php
     if ($isCreatorView ?? null) { ?>
-        <script src="/js/functions.js?v=b3feba6"></script>
+        <script src="/js/functions.js?v=2eac594"></script>
         <?php
         echo '<script>App.functions=App.functions.concat(' . $this->Config->getExtFunctionsTemplates() . ')</script>';
         ?>
         <?php
     } ?>
 
-    <script src="/js/main.js?v=14d625b"></script>
+    <script src="/js/main.js?v=f02cad1"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?17"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

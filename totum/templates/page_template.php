5fa22d81e6372abf391cefed2b97339059462f44
Tatiana <poluninat.m@gmail.com>
2022-07-22 12:13:55 +0300

    окошко linkToDataHtml почему-то залипло внутри iframe

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=6abb4fe"></script>
+    <script src="/js/main.js?v=cfaa7ba"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?18"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

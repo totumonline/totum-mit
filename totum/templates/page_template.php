bb4fb292bc42e3dad76ce6f56725211451cff033
Tatiana <poluninat.m@gmail.com>
2022-07-25 13:15:02 +0300

    в панели улетает датапикер

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=c337840"></script>
+    <script src="/js/main.js?v=ea7e689"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?18"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

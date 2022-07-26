a92f215d28e5876422d9a2de3699a6bdc49be014
Tatiana <poluninat.m@gmail.com>
2022-07-26 17:05:51 +0300

    fx padding

--- totum/moduls/Forms/__Main.php
+++ totum/moduls/Forms/__Main.php
@@ -48,7 +48,7 @@
 <div id="form">
     <script>
         window.MAIN_HOST_FORM = true;
-        let num = "85";
+        let num = "86";
         (function (src, cssSrc, address, post, get, input) {
             let div = document.currentScript.parentNode;
             let path = "<?=$path?>";

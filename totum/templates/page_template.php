0a9f767e1cad710a05ff96bf642f2825b1dd4441
Tatiana <poluninat.m@gmail.com>
2022-07-27 12:34:10 +0300

    colorize cudes btn if table form exists

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=d68aa73">
+          href="/css/main.css?v=917cff4">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=f7b433e"></script>
+    <script src="/js/main.js?v=b7b1175"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?20"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=d68aa73">
-    <script src="/js/main.js?v=f7b433e"></script>
+          href="/css/main.css?v=917cff4">
+    <script src="/js/main.js?v=b7b1175"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?20"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

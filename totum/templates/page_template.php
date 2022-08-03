6c5eb0acdb43e88eeb9c452be77e4aa95d98929f
Tatiana <poluninat.m@gmail.com>
2022-08-03 18:07:29 +0300

    fx moving focus in insert row

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=b180d51"></script>
+    <script src="/js/main.js?v=fd48b8a"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?20"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

8c6b4fcc8b16417216820e732575c3498ca3f07a
Tatiana <poluninat.m@gmail.com>
2022-08-15 17:11:44 +0300

    restore in treeView & asUrl with null value in string field

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=5377619">
-    <script src="/js/main.js?v=34385aa"></script>
+    <script src="/js/main.js?v=ebdf644"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?21"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

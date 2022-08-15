c04d5419b166c33f335ecef4e5a1d1b6109a4d3d
Tatiana <poluninat.m@gmail.com>
2022-08-15 17:38:34 +0300

    comments font-size change

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=5377619">
+          href="/css/main.css?v=1773943">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=ebdf644"></script>
+    <script src="/js/main.js?v=a3c604f"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?21"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

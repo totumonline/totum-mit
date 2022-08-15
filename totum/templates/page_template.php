809bb1770bd9c02f0de71772d8847ffeab8b9ce1
Tatiana <poluninat.m@gmail.com>
2022-08-15 18:11:52 +0300

    textasvalue num in summarizer

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=2d82312"></script>
+    <script src="/js/main.js?v=d704096"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?21"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

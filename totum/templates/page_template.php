c455c574912b2e71dfee16cf8a17af5e5996eab5
Tatiana <poluninat.m@gmail.com>
2022-08-16 18:13:41 +0300

    SearchArea padding

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=c096ad5">
+          href="/css/main.css?v=0868e19">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=155422a"></script>
+    <script src="/js/main.js?v=3077080"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?21"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

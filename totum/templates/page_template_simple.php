1635a26050267b9a70938aaf2f2bfb3752ae40c1
Tatiana <poluninat.m@gmail.com>
2022-08-16 18:10:36 +0300

    mobile css pixels

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=98410fb">
-    <script src="/js/main.js?v=54918a4"></script>
+          href="/css/main.css?v=c096ad5">
+    <script src="/js/main.js?v=155422a"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?21"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

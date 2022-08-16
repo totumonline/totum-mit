721a3b432e2844d13bb73c4fb41176cca1592f8e
Tatiana <poluninat.m@gmail.com>
2022-08-16 13:58:00 +0300

    links color

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=2b8a1c5"></script>
+    <script src="/js/main.js?v=f92f6a6"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?21"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

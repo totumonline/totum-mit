c2dc04ce622d2b3f1db319a114cdb4dfec0147e0
Tatiana <poluninat.m@gmail.com>
2022-08-16 13:13:36 +0300

    fieldhide: 'id' = true in CreatorView magenta colored

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=1773943">
+          href="/css/main.css?v=381222e">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=c381eac"></script>
+    <script src="/js/main.js?v=6a72f49"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?21"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=1773943">
-    <script src="/js/main.js?v=c381eac"></script>
+          href="/css/main.css?v=381222e">
+    <script src="/js/main.js?v=6a72f49"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?21"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

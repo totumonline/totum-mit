57fccd7033ef1a0fc9690f7d8c9bdb6cf6987211
Tatiana <poluninat.m@gmail.com>
2022-08-16 13:36:50 +0300

    textasvalue fx

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=6a72f49"></script>
+    <script src="/js/main.js?v=57a23c0"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?21"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

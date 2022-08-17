3bba6d9ce44f2fae633ab2b2a6056e3b28bc5cb2
Tatiana <poluninat.m@gmail.com>
2022-08-17 18:14:45 +0300

    convert array data in uniq to null

--- totum/fieldTypes/Unic.php
+++ totum/fieldTypes/Unic.php
@@ -20,8 +20,8 @@ class Unic extends Field
         parent::addViewValues($viewType, $valArray, $row, $tbl);
         if (!is_null($valArray['v'])) {
             if ($viewType == 'web') {
-                if(is_array($valArray['v'])){
-                    $valArray['e']=$this->translate('Field data type error');
+                if (is_array($valArray['v'])) {
+                    $valArray['e'] = $this->translate('Field data type error');
                 }
             }
         }
@@ -29,39 +29,44 @@ class Unic extends Field
 
     protected function checkValByType(&$val, $row, $isCheck = false)
     {
-        if (!$isCheck && !is_null($val) && $val !== '') {
-
-            if (!empty($this->data['regexp']) && !preg_match(
-                    '/' . str_replace(
-                        '/',
-                        '\/',
-                        $this->data['regexp']
-                    ) . '/u',
-                    $val
-                )
-            ) {
-                errorException::criticalException(
-                    $this->data['regexpErrorText'] ?? $this->translate('The value of %s field must match the format: %s',
-                        [$this->data['title'], $this->data['regexp']]),
-                    $this->table
-                );
+        if (!$isCheck) {
+            if (is_array($val)) {
+                $val = null;
             }
+            if (!is_null($val) && $val !== '') {
 
-            $where = [
-                ['field' => $this->data['name'], 'operator' => '=', 'value' => $val]
-            ];
-            if ($row['id'] ?? null) {
-                $where[] = ['field' => 'id', 'operator' => '!=', 'value' => $row['id']];
-            }
+                if (!empty($this->data['regexp']) && !preg_match(
+                        '/' . str_replace(
+                            '/',
+                            '\/',
+                            $this->data['regexp']
+                        ) . '/u',
+                        $val
+                    )
+                ) {
+                    errorException::criticalException(
+                        $this->data['regexpErrorText'] ?? $this->translate('The value of %s field must match the format: %s',
+                            [$this->data['title'], $this->data['regexp']]),
+                        $this->table
+                    );
+                }
 
-            if ($id_duble = $this->table->getByParams(
-                ['field' => 'id', 'where' => $where]
-            )) {
-                errorException::criticalException(
-                    $this->translate('The value must be unique. Duplication in rows: [[%s - %s]]',
-                        [$id_duble, $row['id']]),
-                    $this->table
-                );
+                $where = [
+                    ['field' => $this->data['name'], 'operator' => '=', 'value' => $val]
+                ];
+                if ($row['id'] ?? null) {
+                    $where[] = ['field' => 'id', 'operator' => '!=', 'value' => $row['id']];
+                }
+
+                if ($id_duble = $this->table->getByParams(
+                    ['field' => 'id', 'where' => $where]
+                )) {
+                    errorException::criticalException(
+                        $this->translate('The value must be unique. Duplication in rows: [[%s - %s]]',
+                            [$id_duble, $row['id']]),
+                        $this->table
+                    );
+                }
             }
         }
     }

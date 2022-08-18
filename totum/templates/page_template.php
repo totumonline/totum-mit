4f7d524ddac77d6ce7d17d56493316c745237931
Tatiana <poluninat.m@gmail.com>
2022-08-18 13:38:49 +0300

    big image from panel preview

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=fdb79d4"></script>
+    <script src="/js/main.js?v=33711ac"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?22"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

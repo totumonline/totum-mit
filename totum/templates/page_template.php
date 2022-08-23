380e50163daa552a16b0e50330163ad1450e2c55
Tatiana <poluninat.m@gmail.com>
2022-08-23 18:20:09 +0300

    edited colored field in panel

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=2680fe1">
+          href="/css/main.css?v=c73e47b">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=53ba0db"></script>
+    <script src="/js/main.js?v=b07869f"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?24"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=2680fe1">
-    <script src="/js/main.js?v=53ba0db"></script>
+          href="/css/main.css?v=c73e47b">
+    <script src="/js/main.js?v=b07869f"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?24"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

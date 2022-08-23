1e93b75d7b5e95df04ba3d06d7369d9df9ea845f
Tatiana <poluninat.m@gmail.com>
2022-08-23 15:23:13 +0300

    icons change

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=f066026"></script>
+    <script src="/js/main.js?v=20c77bc"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?24"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

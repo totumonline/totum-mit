ea11dd403e93d98fd6034601fa7dc4e8eb5c2428
Tatiana <poluninat.m@gmail.com>
2022-09-02 16:26:20 +0300

    buttons and filter in scroll table head

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=26dc232"></script>
+    <script src="/js/main.js?v=44fb50c"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?24"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=b96e59f">
-    <script src="/js/main.js?v=26dc232"></script>
+    <script src="/js/main.js?v=44fb50c"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?24"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

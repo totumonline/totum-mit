6f199e61d8b92bf205050d281107ca8af35a9a55
Tatiana <poluninat.m@gmail.com>
2022-09-21 15:46:12 +0300

    fx filter on hand values

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=92a4e07">
-    <script src="/js/main.js?v=2ac2ad8"></script>
+    <script src="/js/main.js?v=2d9e9b9"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?25"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

4ff15dcf05288489634e0fcfa0e58860b35e335b
Tatiana <poluninat.m@gmail.com>
2022-09-26 17:07:16 +0300

    js currency format of very big numbers

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=92a4e07">
-    <script src="/js/main.js?v=41dd47e"></script>
+    <script src="/js/main.js?v=9e2de44"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?25"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

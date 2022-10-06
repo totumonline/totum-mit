7839ab4e90b3767e2ac2213449569ffcb33fc573
Tatiana <poluninat.m@gmail.com>
2022-10-06 16:31:35 +0300

    file icons

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=6758ba1">
-    <script src="/js/main.js?v=440f989"></script>
+    <script src="/js/main.js?v=73433b9"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?26"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

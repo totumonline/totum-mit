0b1e5e66c99c95631a96c9a38b0eabd5ce97414c
Tatiana <poluninat.m@gmail.com>
2022-10-18 18:37:14 +0300

    fx float error

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=cb261fd"></script>
+    <script src="/js/main.js?v=2238851"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?27"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

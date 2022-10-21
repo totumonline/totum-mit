f0f32e60cce3aa0a6aaee2cd5a37c5351824156f
Tatiana <poluninat.m@gmail.com>
2022-10-21 14:46:42 +0300

    for PRO theme colorizing

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=ee273a8">
+          href="/css/main.css?v=1646e8d">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=b3ee869"></script>
+    <script src="/js/main.js?v=a6a39c2"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?27"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

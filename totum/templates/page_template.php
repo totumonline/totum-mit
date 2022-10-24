ff98b89ce42149fa8820cb2f873302bf9455406a
Tatiana <poluninat.m@gmail.com>
2022-10-24 17:35:33 +0300

    fx asUrl

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=4f27362"></script>
+    <script src="/js/main.js?v=1eee5dd"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?27"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=ec98605">
-    <script src="/js/main.js?v=4f27362"></script>
+    <script src="/js/main.js?v=1eee5dd"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?27"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

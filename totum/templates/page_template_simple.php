207eb9cabe2f9b391bfa9d0ef2522463ae447972
Tatiana <poluninat.m@gmail.com>
2022-10-27 17:56:14 +0300

    fx focus errors

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=b2052e2">
-    <script src="/js/main.js?v=154cc86"></script>
+    <script src="/js/main.js?v=6bfe0f5"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?28"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

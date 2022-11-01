99905f9ec532949a8ab2b832365810bde2555be0
Tatiana <poluninat.m@gmail.com>
2022-11-01 15:25:22 +0300

    dateIntervals function

--- totum/common/calculates/FuncDatesTrait.php
+++ totum/common/calculates/FuncDatesTrait.php
@@ -107,6 +107,88 @@ trait FuncDatesTrait
         return $this->dateFormat(date_create(), ($params['format'] ?? 'Y-m-d H:i'), $params['lang'] ?? null);
     }
 
+    protected function funcDateIntervals($params)
+    {
+        $params = $this->getParamsArray($params);
+        $this->__checkNotEmptyParams($params, ['date', 'type']);
+        $this->__checkNumericParam($params['num'] ?? null, 'num');
+        $this->__checkNotArrayParams($params, ['format', 'date']);
+        $date = $this->__checkGetDate($params['date'], 'date', 'dateIntervals');
+        $dateTime = match ($params['datetime'] ?? false) {
+            true, 'true' => true,
+            default => false
+        };
+
+        $result = [];
+
+        switch ($params['type']) {
+            case 'hour':
+                $func = function ($start) use (&$result, $params) {
+                    $row = [];
+                    $row['start'] = clone $start;
+                    $start->modify('+ 1 hour');
+                    $row['end'] = (clone $start)->modify('-1 sec');
+                    return (object)$row;
+                };
+                $start = $date->modify('-' . $date->format('i') . 'minutes - ' . $date->format('s') . ' sec');
+                break;
+            case 'day':
+                $func = function ($start) use (&$result, $params) {
+                    $row = [];
+                    $row['start'] = clone $start;
+                    $start->modify('+ 1 day');
+                    $row['end'] = (clone $start)->modify('-1 sec');
+                    return (object)$row;
+                };
+                $start = $date->modify('-' . $date->format('H') . 'hours -' . $date->format('i') . 'minutes - ' . $date->format('s') . ' sec');
+                break;
+            case 'week':
+                $func = function ($start) use (&$result, $params) {
+                    $row = [];
+                    $row['start'] = clone $start;
+                    $start->modify('+ 7 day');
+                    $row['end'] = (clone $start)->modify('-1 sec');
+                    return (object)$row;
+                };
+                $start = $date->modify('-' . ($date->format('N') - 1) . ' days -' . $date->format('H') . 'hours -' . $date->format('i') . 'minutes - ' . $date->format('s') . ' sec');
+                break;
+            case 'month':
+                $func = function ($start) use (&$result, $params) {
+                    $row = [];
+                    $row['start'] = clone $start;
+                    $start->modify('+ ' . $start->format('t') . ' day');
+                    $row['end'] = (clone $start)->modify('-1 sec');
+                    return (object)$row;
+                };
+                $start = date_create($date->format('Y') . '-' . $date->format('m') . '-01 00:00:00');
+                break;
+            case 'year':
+                $func = function ($start) use (&$result, $params) {
+                    $row = [];
+                    $row['start'] = clone $start;
+                    $start->modify('+ 1 year');
+                    $row['end'] = (clone $start)->modify('-1 sec');
+                    return (object)$row;
+                };
+                $start = date_create($date->format('Y') . '-01-01 00:00:00');
+                break;
+        };
+
+        $defaultFormat = $dateTime ? 'Y-m-d H:i' : 'Y-m-d';
+        for ($i = 0; $i < $params['num']; $i++) {
+            $_ = $func($start);
+            $row = ['start' => $_->start->format($defaultFormat), 'end' => $_->end->format($defaultFormat)];
+            if (!empty($params['format'])) {
+                $row['startf'] = $_->start->format($params['format']);
+                $row['endf'] = $_->end->format($params['format']);
+            }
+            $result[] = $row;
+        }
+
+        return $result;
+
+    }
+
     private function diffDates(DateTime $date1, DateTime $date2, $unit): float|int
     {
         switch ($unit) {
--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -11,14 +11,14 @@
 
     <?php
     if ($isCreatorView ?? null) { ?>
-        <script src="/js/functions.js?v=a71a92d"></script>
+        <script src="/js/functions.js?v=8666283"></script>
         <?php
         echo '<script>App.functions=App.functions.concat(' . $this->Config->getExtFunctionsTemplates() . ')</script>';
         ?>
         <?php
     } ?>
 
-    <script src="/js/main.js?v=6bfe0f5"></script>
+    <script src="/js/main.js?v=837f8ce"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?28"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

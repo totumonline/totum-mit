71d69f425a9474edc86ba6edcdc9bdb1a4a297f8
Tatiana <poluninat.m@gmail.com>
2022-11-03 14:46:06 +0300

    fx file field dropdown accept

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,8 +18,8 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=837f8ce"></script>
-    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?28"></script>
+    <script src="/js/main.js?v=ab2f038"></script>
+    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?29"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
 

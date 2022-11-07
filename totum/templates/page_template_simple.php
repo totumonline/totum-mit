77b224e238a3a23c33a79c1bf6f36b25f514c133
Tatiana <poluninat.m@gmail.com>
2022-11-07 16:10:48 +0300

    fx file accept

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=b2052e2">
-    <script src="/js/main.js?v=8bd362b"></script>
+    <script src="/js/main.js?v=3a095f6"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?29"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

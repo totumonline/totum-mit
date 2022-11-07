7f495fc928424f4a8d884005e68083e3dec307e8
Tatiana <poluninat.m@gmail.com>
2022-11-07 16:51:49 +0300

    refresh : reload

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=3a095f6"></script>
+    <script src="/js/main.js?v=6318587"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?29"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

089e1e2957aab2fa1f2893aad9af97ab7a5d91ce
Tatiana <poluninat.m@gmail.com>
2022-11-10 15:22:17 +0300

    pagination colorizer

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=2a96248"></script>
+    <script src="/js/main.js?v=96c52c5"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?29"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

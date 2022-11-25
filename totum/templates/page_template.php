7d48a7caeabcdf593c6a6bdf6faf1a6fbf3ca199
Tatiana <poluninat.m@gmail.com>
2022-11-25 15:37:59 +0300

    strUrlEncode && strUrlDecode

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -11,14 +11,14 @@
 
     <?php
     if ($isCreatorView ?? null) { ?>
-        <script src="/js/functions.js?v=05b3faf"></script>
+        <script src="/js/functions.js?v=9ccfdcb"></script>
         <?php
         echo '<script>App.functions=App.functions.concat(' . $this->Config->getExtFunctionsTemplates() . ')</script>';
         ?>
         <?php
     } ?>
 
-    <script src="/js/main.js?v=2ce4dbd"></script>
+    <script src="/js/main.js?v=fbf1a38"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?1"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

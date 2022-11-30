4b149f1c893789a2a26e99239c7f7392c4803da0
Tatiana <poluninat.m@gmail.com>
2022-11-30 13:35:23 +0300

    fx services_vars cron error

--- totum/commands/CleanSchemaTmpTables.php
+++ totum/commands/CleanSchemaTmpTables.php
@@ -8,6 +8,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use totum\common\configs\MultiTrait;
+use totum\common\Services\Services;
 use totum\config\Conf;
 
 class CleanSchemaTmpTables extends Command
@@ -38,7 +39,14 @@ class CleanSchemaTmpTables extends Command
 
         $minusHour = date_create();
         $minusHour->modify('-1 hour');
-        $sql->exec('delete from _services_vars where expire<\'' . $minusHour->format('Y-m-d H:i:s') . '\'');
+        try {
+            $sql->exec('delete from _services_vars where expire<\'' . $minusHour->format('Y-m-d H:i:s') . '\'');
+        }catch (\Exception $exception){
+            if($exception->getCode()==='42P01'){
+                $Services = Services::init($Conf);
+                $Services->createServicesTable();
+            }
+        }
 
         $minus10 = date_create();
         $minus10->modify('-30 minutes');

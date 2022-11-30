42e6841bd32e7d28c184f863903644d0739e498c
Tatiana <poluninat.m@gmail.com>
2022-11-30 14:30:41 +0300

    up auth_form shadow; fx "border: 1: false" with field background

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=7aa54b9">
+          href="/css/main.css?v=18c9459">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=cc10ec7"></script>
+    <script src="/js/main.js?v=28f397d"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?1"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

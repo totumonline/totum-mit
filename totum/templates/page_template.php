cbddfbd67b576ced5076a346e09ab1942173afcd
Tatiana <poluninat.m@gmail.com>
2022-12-02 14:41:02 +0300

    fx math operations with textasvalue cells

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=beaf06f"></script>
+    <script src="/js/main.js?v=84b58d0"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?1"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=7b11da1">
-    <script src="/js/main.js?v=beaf06f"></script>
+    <script src="/js/main.js?v=84b58d0"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?1"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

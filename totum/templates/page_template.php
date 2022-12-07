bf54ba8f66d7ba92825014246d22efd4d440eeda
Tatiana <poluninat.m@gmail.com>
2022-12-07 16:25:12 +0300

    switching to desktop interface version on small screen warning

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,8 +18,8 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=84b58d0"></script>
-    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?1"></script>
+    <script src="/js/main.js?v=b9d7da6"></script>
+    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,8 +8,8 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=7b11da1">
-    <script src="/js/main.js?v=84b58d0"></script>
-    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?1"></script>
+    <script src="/js/main.js?v=b9d7da6"></script>
+    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>

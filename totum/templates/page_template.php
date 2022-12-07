26a6834f28f35ff57b39292e86da8bfd9d648e73
Tatiana <poluninat.m@gmail.com>
2022-12-07 16:38:12 +0300

    reload on switching in normal mode

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=b9d7da6"></script>
+    <script src="/js/main.js?v=8db631c"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=7b11da1">
-    <script src="/js/main.js?v=b9d7da6"></script>
+    <script src="/js/main.js?v=8db631c"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

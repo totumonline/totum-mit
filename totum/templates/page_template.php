1c000989ee3187e39cd6ec5aac007fc4d1159586
Tatiana <poluninat.m@gmail.com>
2022-12-15 18:42:18 +0300

    changes edit elements in insertRow

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=7283aee"></script>
+    <script src="/js/main.js?v=d651945"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

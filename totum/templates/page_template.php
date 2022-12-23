1fd6d0c148241c1df461c458085f044d37911c89
Tatiana <poluninat.m@gmail.com>
2022-12-23 16:24:03 +0300

    fx floatBlocks

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=3faa895"></script>
+    <script src="/js/main.js?v=554ca58"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=da35c28">
-    <script src="/js/main.js?v=3faa895"></script>
+    <script src="/js/main.js?v=554ca58"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

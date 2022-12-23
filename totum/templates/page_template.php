b321dc057c6e3f1631646a0dee021244b1e84196
Tatiana <poluninat.m@gmail.com>
2022-12-23 15:49:54 +0300

    fx floatBlocks; platemaxdiff

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=7b11da1">
+          href="/css/main.css?v=da35c28">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=f952f1e"></script>
+    <script src="/js/main.js?v=3faa895"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=7b11da1">
-    <script src="/js/main.js?v=f952f1e"></script>
+          href="/css/main.css?v=da35c28">
+    <script src="/js/main.js?v=3faa895"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

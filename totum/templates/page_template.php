fb3f8e72529d594ec295581c4eae58491a59e863
Tatiana <poluninat.m@gmail.com>
2022-12-26 18:04:11 +0300

    common-table-title admin-hidden

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=0ff2d83">
+          href="/css/main.css?v=8371ef5">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=5df9047"></script>
+    <script src="/js/main.js?v=724629d"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

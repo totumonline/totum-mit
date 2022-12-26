536ab6077c92e7a8b145afafe1ede9393a720e56
Tatiana <poluninat.m@gmail.com>
2022-12-26 15:58:40 +0300

    fonts rm Roboto

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=da35c28">
+          href="/css/main.css?v=0ff2d83">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=554ca58"></script>
+    <script src="/js/main.js?v=5df9047"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=da35c28">
-    <script src="/js/main.js?v=554ca58"></script>
+          href="/css/main.css?v=0ff2d83">
+    <script src="/js/main.js?v=5df9047"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

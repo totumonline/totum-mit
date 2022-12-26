e102a2bb849c1012c4b6ba55f2336627a9e544aa
Tatiana <poluninat.m@gmail.com>
2022-12-26 18:08:15 +0300

    common-table-title admin-hidden

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=724629d"></script>
+    <script src="/js/main.js?v=606b65d"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

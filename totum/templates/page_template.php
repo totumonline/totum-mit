7398c9d372f7445f4e1ea8afba212029444bc6bb
Tatiana <poluninat.m@gmail.com>
2022-12-28 15:07:48 +0300

    focus on add button after it was pressed on add button

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=ad7ca87"></script>
+    <script src="/js/main.js?v=9068e6b"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

2f3a80ee7bd9c94b6c7f2da22e6c74ff2f753d65
Tatiana <poluninat.m@gmail.com>
2022-12-29 18:31:36 +0300

    footer select moving etc

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=243bb9e"></script>
+    <script src="/js/main.js?v=f0ee479"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

93d79d64b7d01ebb8932b9e8ef665df03a97a8f3
Tatiana <poluninat.m@gmail.com>
2022-12-29 16:54:10 +0300

    selecting by arrows

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=8371ef5">
+          href="/css/main.css?v=7ea8e52">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=f0cbd9c"></script>
+    <script src="/js/main.js?v=23c86a3"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

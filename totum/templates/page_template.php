4fb0c6c32e3d142adaf47c0535819a52dcf375a3
Tatiana <poluninat.m@gmail.com>
2022-12-30 18:03:22 +0300

    shift+enter = click on selected button

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=3d7fb0f"></script>
+    <script src="/js/main.js?v=a0bff4f"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

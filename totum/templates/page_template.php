681906139b78785c63198b69527122deb83edd4e
Tatiana <poluninat.m@gmail.com>
2022-12-30 17:41:09 +0300

    fx editing by button with opened panels

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=4cd81e3"></script>
+    <script src="/js/main.js?v=f8fe7c8"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

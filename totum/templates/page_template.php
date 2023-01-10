4e8f988781febf427d40a7806f472b55667ded06
Tatiana <poluninat.m@gmail.com>
2023-01-10 17:42:26 +0300

    fx summarizer for textasvalue; front inprvs

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=18ac6ff"></script>
+    <script src="/js/main.js?v=9b149aa"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

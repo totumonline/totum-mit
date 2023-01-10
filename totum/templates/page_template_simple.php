bfae5636f806c3cbd93a2afe011ea43c7e7bd7b0
Tatiana <poluninat.m@gmail.com>
2023-01-10 16:15:25 +0300

    front: hot keys

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=7ea8e52">
-    <script src="/js/main.js?v=ac87996"></script>
+    <script src="/js/main.js?v=248a795"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

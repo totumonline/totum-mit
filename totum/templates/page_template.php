9626a2b842699155488eace9c0772162aca3f741
Tatiana <poluninat.m@gmail.com>
2023-01-10 15:45:14 +0300

    front: [S+E] [C+E] [A+E] [A+S+E] in header and footer

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=8b50ad9"></script>
+    <script src="/js/main.js?v=ac87996"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

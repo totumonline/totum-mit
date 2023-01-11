43c72c78a4b6fb2d1de0c3f49ddf1f78e7a46030
Tatiana <poluninat.m@gmail.com>
2023-01-11 14:23:42 +0300

    fx tablehtml

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=9b149aa"></script>
+    <script src="/js/main.js?v=ddab1c9"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

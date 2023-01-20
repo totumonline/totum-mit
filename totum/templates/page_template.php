0dee7f38e0ad9e7454e7a35219884930efedd407
Tatiana <poluninat.m@gmail.com>
2023-01-20 18:34:00 +0300

    shift+arrows

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=654b331"></script>
+    <script src="/js/main.js?v=32ad950"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

f0f8a5149ecac53245ef533c7834e26a328a7fc6
Tatiana <poluninat.m@gmail.com>
2023-01-23 11:01:42 +0300

    pressing esc for deselect selected cells

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=eb29711">
-    <script src="/js/main.js?v=09fe6f2"></script>
+    <script src="/js/main.js?v=ae1bbbf"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

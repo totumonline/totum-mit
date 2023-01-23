389fe0ae1b7dac669b7bc895a9f9dc48942a7f7e
Tatiana <poluninat.m@gmail.com>
2023-01-23 13:53:09 +0300

    fx multi editing file fields

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=eb29711">
-    <script src="/js/main.js?v=ae1bbbf"></script>
+    <script src="/js/main.js?v=bfcb739"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

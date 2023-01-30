892719ea57d586abd5b2f9c23f8b95a807e49fdb
Tatiana <poluninat.m@gmail.com>
2023-01-30 16:42:36 +0300

    fx 2 px

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=eb29711">
-    <script src="/js/main.js?v=b4139f9"></script>
+          href="/css/main.css?v=308c6bd">
+    <script src="/js/main.js?v=3d4bc0e"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

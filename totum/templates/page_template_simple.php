885865d481bafb0291d63af3a29e9887cc460107
Tatiana <poluninat.m@gmail.com>
2023-01-30 17:02:44 +0300

    fx warningEditText translation

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=308c6bd">
-    <script src="/js/main.js?v=3d4bc0e"></script>
+    <script src="/js/main.js?v=f54edc1"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

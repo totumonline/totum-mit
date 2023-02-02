0e0aacff76bf06b89fc1b0befbf155b1bf0ed121
Tatiana <poluninat.m@gmail.com>
2023-02-02 14:30:16 +0300

    upd front pointer

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=d01ee51"></script>
+    <script src="/js/main.js?v=a664fb3"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

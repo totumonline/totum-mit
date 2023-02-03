37f4e45e290cd73d8a7a319e952756d5cf238184
Tatiana <poluninat.m@gmail.com>
2023-02-03 15:07:33 +0300

    mark switched-off codes in edit window

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=308c6bd">
+          href="/css/main.css?v=2c1e871">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=c26a96b"></script>
+    <script src="/js/main.js?v=5c46b98"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

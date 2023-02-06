fb447e1c97257994d4a6d84ba72e8f6b29f1b47f
Tatiana <poluninat.m@gmail.com>
2023-02-06 17:10:38 +0300

    switch on code-completing for "#" in "green codes"

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=a76a52d"></script>
+    <script src="/js/main.js?v=79b850d"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

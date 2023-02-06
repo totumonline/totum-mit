04eb2965bac8f85a4b967c3c9d93cc72a084aa83
Tatiana <poluninat.m@gmail.com>
2023-02-06 17:40:25 +0300

    copyOnDuplicate saves value in InsertRow by past-button

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=2c1e871">
-    <script src="/js/main.js?v=49bc4dd"></script>
+    <script src="/js/main.js?v=634b6a5"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

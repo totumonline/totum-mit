0b719f80442720440db5f93daec4574ff0cf0907
Tatiana <poluninat.m@gmail.com>
2023-02-06 17:30:15 +0300

    panelButton codeEdit opens now actionCode

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=79b850d"></script>
+    <script src="/js/main.js?v=49bc4dd"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

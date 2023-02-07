a1d60f5e4ac4bf0b651db0e5b2224817cb2f86c7
Tatiana <poluninat.m@gmail.com>
2023-02-07 17:28:01 +0300

    isMobile by screen && css media

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,8 +18,8 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=634b6a5"></script>
-    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
+    <script src="/js/main.js?v=d461ef5"></script>
+    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,8 +8,8 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=2c1e871">
-    <script src="/js/main.js?v=634b6a5"></script>
-    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
+    <script src="/js/main.js?v=d461ef5"></script>
+    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>

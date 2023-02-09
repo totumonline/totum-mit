b294807d80e85cd336379e85a1e160ec48c9477a
Tatiana <poluninat.m@gmail.com>
2023-02-09 16:24:56 +0300

    moved mobile/descktop switcher to user menu

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=299e6fc"></script>
+    <script src="/js/main.js?v=80b2c0a"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=cf1ce4e">
-    <script src="/js/main.js?v=299e6fc"></script>
+    <script src="/js/main.js?v=80b2c0a"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

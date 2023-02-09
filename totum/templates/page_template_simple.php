132141be008f473f95304d3dd8e53b0bde44be31
Tatiana <poluninat.m@gmail.com>
2023-02-09 15:10:40 +0300

    fx mobile switcher in admin view; fx margin in float blocks with title

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=2d1f865">
-    <script src="/js/main.js?v=cbdbdce"></script>
+    <script src="/js/main.js?v=b51dbe2"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

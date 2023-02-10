17fc3e88e57c93e087f4694f205cc9d45f772205
Tatiana <poluninat.m@gmail.com>
2023-02-10 16:50:51 +0300

    userFio popover - only bottom

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=874307d"></script>
+    <script src="/js/main.js?v=8ade8c7"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

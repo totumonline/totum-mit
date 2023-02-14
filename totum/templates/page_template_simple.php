18ed6676c19f89d10b96df0c3a0ff13dce3d3a40
Tatiana <poluninat.m@gmail.com>
2023-02-14 15:30:57 +0300

    fx error with edit select src in edit/insert panel

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=cf1ce4e">
-    <script src="/js/main.js?v=d701e62"></script>
+    <script src="/js/main.js?v=43ff757"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

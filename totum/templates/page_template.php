2fa74061eec48b1e7eeb439b81fc455e2085a825
Tatiana <poluninat.m@gmail.com>
2023-02-15 14:19:07 +0300

    fx linktoinput focus

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=43ff757"></script>
+    <script src="/js/main.js?v=b3846a6"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

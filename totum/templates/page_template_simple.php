3efbe4659c253537ed624078ef59a9add670c87f
Tatiana <poluninat.m@gmail.com>
2023-02-15 14:52:32 +0300

    fx user login panel buttons

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=cf1ce4e">
-    <script src="/js/main.js?v=b3846a6"></script>
+    <script src="/js/main.js?v=d0e7bab"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

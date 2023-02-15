5b9f4624ad95cda484724feee7ddd5694bb3f30c
Tatiana <poluninat.m@gmail.com>
2023-02-15 15:08:30 +0300

    fx multiplicating format text in editPanel

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=d0e7bab"></script>
+    <script src="/js/main.js?v=1f7a7b9"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

6378a15ad1d204130d183cf3921265ad9cffb8de
Tatiana <poluninat.m@gmail.com>
2023-02-16 18:02:48 +0300

    rm "without topbuttons" for opened by iframed button Tab tables

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=55a9c07"></script>
+    <script src="/js/main.js?v=2614679"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?4"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

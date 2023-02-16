28fec8f1acfbd51146c657c8ce79630e4dbf5579
Tatiana <poluninat.m@gmail.com>
2023-02-16 16:50:36 +0300

    fx jumping select in insertRow

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=8df8f25"></script>
+    <script src="/js/main.js?v=d76a3fb"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?4"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

1fd9ecf9b31d825d87f937fadc1af7c18f413d4c
Tatiana <poluninat.m@gmail.com>
2023-02-17 17:54:26 +0300

    fx notification clock position

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=3f31723">
-    <script src="/js/main.js?v=472497e"></script>
+          href="/css/main.css?v=7b76a9a">
+    <script src="/js/main.js?v=fb4cebe"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?4"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

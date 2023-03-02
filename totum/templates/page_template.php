5a9bf1da1063aab3e5809e5586af40642fe2c14f
Tatiana <poluninat.m@gmail.com>
2023-03-02 15:40:55 +0300

    print pdf translate

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=bde0150"></script>
+    <script src="/js/main.js?v=697de93"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?6"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=2069806">
-    <script src="/js/main.js?v=bde0150"></script>
+    <script src="/js/main.js?v=697de93"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?6"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

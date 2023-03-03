a78d070c57f2de46e6f827e587e42c182ea84008
Tatiana <poluninat.m@gmail.com>
2023-03-03 19:19:09 +0300

    rightbottombutton

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=0b8940b"></script>
+    <script src="/js/main.js?v=a70eef2"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?6"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=421e5c3">
-    <script src="/js/main.js?v=0b8940b"></script>
+    <script src="/js/main.js?v=a70eef2"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?6"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

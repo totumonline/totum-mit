d6c378e9d428388887d3f7ab990c4599ccce35a5
Tatiana <poluninat.m@gmail.com>
2023-03-03 19:55:47 +0300

    rightbottombutton

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=421e5c3">
+          href="/css/main.css?v=27974bb">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=c5d0a64"></script>
+    <script src="/js/main.js?v=613f57f"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?6"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

abac46ffefbe003124dec7a3880f3af288a6e8f3
Tatiana <poluninat.m@gmail.com>
2023-03-07 14:52:14 +0300

    saving selection in table pdf print form

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=b92b982">
-    <script src="/js/main.js?v=61e237c"></script>
+    <script src="/js/main.js?v=7a3399d"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?6"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

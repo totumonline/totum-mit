f6138c7dae7a468a0cfc1a12b9ef3299a591945a
Tatiana <poluninat.m@gmail.com>
2023-03-08 18:43:15 +0300

    switch on nicescroll for an tables

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=de57225"></script>
+    <script src="/js/main.js?v=56a030c"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?6"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

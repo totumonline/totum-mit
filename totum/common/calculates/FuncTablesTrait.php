174441b32ef656e4b5ccd76ff73a96b81384790f
Tatiana <poluninat.m@gmail.com>
2023-03-09 14:28:16 +0300

    getUsingFields: add search in fields format

--- totum/common/calculates/FuncTablesTrait.php
+++ totum/common/calculates/FuncTablesTrait.php
@@ -79,7 +79,8 @@ trait FuncTablesTrait
         $query = <<<SQL
 select table_name->>'v' as table_name, name->>'v' as name, version->>'v' as version from tables_fields where data->'v'->'code'->'==usedFields=='-> :table ->> :field = '1'
            OR data->'v'->'codeSelect'->'==usedFields=='-> :table ->> :field = '1'
-            OR data->'v'->'codeAction'->'==usedFields=='-> :table ->> :field = '1';
+            OR data->'v'->'codeAction'->'==usedFields=='-> :table ->> :field = '1'
+            OR data->'v'->'format'->'==usedFields=='-> :table ->> :field = '1';
 SQL;
 
 

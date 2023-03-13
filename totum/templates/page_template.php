15268d1a79b1480098c22c4dfc51053d5b0bd8a7
Tatiana <poluninat.m@gmail.com>
2023-03-13 17:39:05 +0300

    hide empty pcTable-paramsTables

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=b92b982">
+          href="/css/main.css?v=1cd0eda">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=bcd3262"></script>
+    <script src="/js/main.js?v=ee6d972"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?7"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=b92b982">
-    <script src="/js/main.js?v=bcd3262"></script>
+          href="/css/main.css?v=1cd0eda">
+    <script src="/js/main.js?v=ee6d972"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?7"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

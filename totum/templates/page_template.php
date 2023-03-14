e31ef7f525bb436b6874801d1284e79b5f864b68
Tatiana <poluninat.m@gmail.com>
2023-03-14 16:42:58 +0300

    fx sticking table view

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=a4f64a4"></script>
+    <script src="/js/main.js?v=9088162"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?7"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

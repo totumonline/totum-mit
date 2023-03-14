e8179955e9d1e8b89463818f3022e17beaec8c17
Tatiana <poluninat.m@gmail.com>
2023-03-14 14:54:43 +0300

    dots spinner

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=65c1eba"></script>
+    <script src="/js/main.js?v=e48d6e9"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?7"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

fd086de3e38e5b57e4c56c3c39e9aca2d5f9f3fb
Tatiana <poluninat.m@gmail.com>
2023-03-16 16:36:51 +0300

    editPanel improves

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=712095c"></script>
+    <script src="/js/main.js?v=1ce2c6a"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?7"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

8f339c28a934443e4c3dd775170125bfbf514301
Tatiana <poluninat.m@gmail.com>
2023-03-17 17:47:19 +0300

    fx listRow textasvalue

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=f9dccc7"></script>
+    <script src="/js/main.js?v=a3ccfa1"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?7"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

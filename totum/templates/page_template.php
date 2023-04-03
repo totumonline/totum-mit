736a72822e0001651f40cce29ac0434973242d82
Tatiana <poluninat.m@gmail.com>
2023-04-03 13:22:00 +0300

    fx panelbuttonsclear

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=b2fb40b"></script>
+    <script src="/js/main.js?v=fb581b4"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?7"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

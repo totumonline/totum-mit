b7dd82330778b68f69eae4750d47efd39ac3a6f8
Tatiana <poluninat.m@gmail.com>
2023-04-03 17:55:45 +0300

    fx summarizer in iframes

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=77b77d6">
-    <script src="/js/main.js?v=b54b0a8"></script>
+    <script src="/js/main.js?v=c889fa6"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?7"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

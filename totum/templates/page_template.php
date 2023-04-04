bf4765ac8c5714fa36d08d3473a8a416bca66c13
Tatiana <poluninat.m@gmail.com>
2023-04-04 14:53:31 +0300

    fx js error when refresh row in kanban-view with a non-existent kanban-status

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=5356c0c"></script>
+    <script src="/js/main.js?v=dd26eb8"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?7"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

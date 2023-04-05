8202da8c82f11ae636dc1222742ff7347b815e60
Tatiana <poluninat.m@gmail.com>
2023-04-05 16:07:04 +0300

    fx editPanel save errors

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=ccfac9f"></script>
+    <script src="/js/main.js?v=90e830d"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?7"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

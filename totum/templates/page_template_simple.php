c9a0951af6901a03bd80ff8c7d007c5d31d0f126
Tatiana <poluninat.m@gmail.com>
2023-04-10 15:10:50 +0300

    extra-height for linktoinputselect

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=77b77d6">
-    <script src="/js/main.js?v=b6ef130"></script>
+          href="/css/main.css?v=22f66e4">
+    <script src="/js/main.js?v=f4bd86c"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?7"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

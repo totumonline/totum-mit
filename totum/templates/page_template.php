6435b240b2bbd7416846f2c6d0c8825573eb0418
Tatiana <poluninat.m@gmail.com>
2023-04-12 15:30:51 +0300

    de.js && some translations

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=e765824"></script>
+    <script src="/js/main.js?v=82725d6"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?9"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=754d340">
-    <script src="/js/main.js?v=e765824"></script>
+    <script src="/js/main.js?v=82725d6"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?9"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

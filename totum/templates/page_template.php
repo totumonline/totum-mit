24b797381971be4b554ddd52044e0b387f122d60
Tatiana <poluninat.m@gmail.com>
2023-04-13 14:27:25 +0300

    fx innerContainer width in panelView

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=d00fe90"></script>
+    <script src="/js/main.js?v=33d663e"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?9"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=c757407">
-    <script src="/js/main.js?v=d00fe90"></script>
+    <script src="/js/main.js?v=33d663e"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?9"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

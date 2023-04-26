9beea6e1893d0b7489210a7d621bb3c2f83b9583
Tatiana <poluninat.m@gmail.com>
2023-04-26 15:54:51 +0300

    rotated table view: rm sort button

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=0060f0b">
+          href="/css/main.css?v=5669a1d">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=c7c3be8"></script>
+    <script src="/js/main.js?v=d777cd8"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?9"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

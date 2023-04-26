9f0fc92a95afb5ea48989f9f2dc00681c7f567ce
Tatiana <poluninat.m@gmail.com>
2023-04-26 14:59:29 +0300

    fx creating field errors

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=cef1350"></script>
+    <script src="/js/main.js?v=ceec5a3"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?9"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

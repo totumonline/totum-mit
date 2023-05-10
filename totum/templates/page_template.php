a6ebf81f6f4116d11925c60609faeb7d39c22a00
Tatiana <poluninat.m@gmail.com>
2023-05-10 16:49:40 +0300

    new PRO field params

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=4426d9e"></script>
+    <script src="/js/main.js?v=c40da04"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?9"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

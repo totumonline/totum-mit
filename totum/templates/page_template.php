7e7ac314b5d282c8b5122e3412647fb8a530fdb3
Tatiana <poluninat.m@gmail.com>
2023-05-10 16:06:12 +0300

    new PRO field params

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=cb5b853">
+          href="/css/main.css?v=16267f5">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=3a753ef"></script>
+    <script src="/js/main.js?v=4426d9e"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?9"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

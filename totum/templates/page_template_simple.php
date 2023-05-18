5d138952e43dd3ff4e67b015ff5944d0ba1fc6b8
Tatiana <poluninat.m@gmail.com>
2023-05-18 15:17:03 +0300

    schema && translates update

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,8 +8,8 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=b07be2e">
-    <script src="/js/main.js?v=143bc05"></script>
-    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?9"></script>
+    <script src="/js/main.js?v=4e7c28e"></script>
+    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?1"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>

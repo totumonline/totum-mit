2673bf3b10107ea3f0104e244b70612687a828c0
Tatiana <poluninat.m@gmail.com>
2023-05-19 14:25:55 +0300

    kanban improve; alt+s without shiftKey

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=b07be2e">
+          href="/css/main.css?v=dc22882">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=a307cb7"></script>
+    <script src="/js/main.js?v=a9974e1"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?1"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=c27fdf7"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=b07be2e">
-    <script src="/js/main.js?v=a307cb7"></script>
+          href="/css/main.css?v=dc22882">
+    <script src="/js/main.js?v=a9974e1"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?1"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

d791c50a49d9b4a6e1995421c4282421dd707dba
Tatiana <-->
2023-05-25 12:56:20 +0300

    select input in insert row

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=dc22882">
-    <script src="/js/main.js?v=61cd63d"></script>
+    <script src="/js/main.js?v=d68c0f9"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?1"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

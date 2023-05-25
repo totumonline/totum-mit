31287ed1fa21fe90fcfb552036b201f6fb821611
Tatiana <-->
2023-05-25 15:43:33 +0300

    fx error with not filtering in requesting information when copySelected

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=834b17f"></script>
+    <script src="/js/main.js?v=346fbf2"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?1"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=dc22882">
-    <script src="/js/main.js?v=834b17f"></script>
+    <script src="/js/main.js?v=346fbf2"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?1"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

53c67911c96a142e2f34b8a373abf244fadfce7d
Tatiana <-->
2023-06-07 16:56:28 +0300

    excel print form translates

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -19,7 +19,7 @@
     } ?>
 
     <script src="/js/main.js?v=74af3ae"></script>
-    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?1"></script>
+    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?2"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
 

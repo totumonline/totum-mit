c8ab2528a0f7a4da3408027a46f390194e13f89a
Tatiana <-->
2023-06-13 15:44:12 +0300

    ServiceXlsxParser

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -11,7 +11,7 @@
 
     <?php
     if ($isCreatorView ?? null) { ?>
-        <script src="/js/functions.js?v=7175840"></script>
+        <script src="/js/functions.js?v=64c15ba"></script>
         <?php
         echo '<script>App.functions=App.functions.concat(' . $this->Config->getExtFunctionsTemplates() . ')</script>';
         ?>

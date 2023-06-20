4d6ea83a129e7a88056d5e2bc75b0a197f96e528
Tatiana <-->
2023-06-20 15:57:57 +0300

    fx scroll header in panels view kanban

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=da79501"></script>
+    <script src="/js/main.js?v=e6b6d5d"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=f037941">
-    <script src="/js/main.js?v=da79501"></script>
+    <script src="/js/main.js?v=e6b6d5d"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?3"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

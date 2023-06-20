0bf9574a826d5406bc9d383209349c1cadcf60ab
Tatiana <-->
2023-06-20 15:16:19 +0300

    exleImport tmp table window width

--- totum/moduls/Table/WriteTableActions.php
+++ totum/moduls/Table/WriteTableActions.php
@@ -168,17 +168,20 @@ class WriteTableActions extends ReadTableActions
     {
         if ($this->isTableServiceOn('xlsximport')) {
             $calc = new CalculateAction(<<<CODE
-= : linkToFileUpload(title: $#title; code: \$code; limit: 1; type: ".xlsx"; var: "title" = $#title; var: 'table'=$#table; refresh: true)
+= : linkToFileUpload(title: $#title; code: \$code; limit: 1; type: ".xlsx"; var: "title" = $#title; var: 'table'=$#table; var: 'width'=$#width;  refresh: true)
 ```code:totum
-=: linkToDataTable(table: 'ttm__prepared_data_import'; title: $#title;  params: \$params;  target: "iframe"; width: "90wv"; refresh: true; bottombuttons: 'force')
+=: linkToDataTable(table: 'ttm__prepared_data_import'; title: $#title; width: \$width; params: \$params;  target: "iframe"; refresh: true; bottombuttons: 'force')
 params: rowCreate(field: "h_import_data" = \$fileData; field: "h_table" = $#table; field: "h_iscolumnsinfirstrow"=true)
+width: if(condition: \$columnsWidth > $#width; then: "80wv"; else: \$columnsWidth)
+    ~columnsWidth: listCount(list: \$fileData[0]) * 100 + 100
 ~fileData: serviceXlsxParser(filestring: $#input[0][filestring]; withformats: false; withcolumns: true)
 ```
 CODE
             );
             $calc->execAction('CODE', [], [], [], [], $this->Table, 'exec', [
                 'title' => $this->translate('Excel import to %s', $this->post['title']),
-                'table' => $this->Table->getTableRow()['name']
+                'table' => $this->Table->getTableRow()['name'],
+                'width'=> 0.8 * $this->post['bwidth']
             ]);
 
         } else {

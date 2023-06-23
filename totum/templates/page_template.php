dceed25466cbd811d9af2db5138b9127e46ec08e
Tatiana <-->
2023-06-23 14:39:30 +0300

    add row buttons panel dynamic width

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=55928e0"></script>
+    <script src="/js/main.js?v=11d6800"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?4"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

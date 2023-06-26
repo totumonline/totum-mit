d5945574207f8b971f69de8f0d87c52ad8d31697
Tatiana <-->
2023-06-26 16:43:56 +0300

    fx treeView tree changes

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=f9bea7a"></script>
+    <script src="/js/main.js?v=5a79893"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?4"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

15803d6c0e0bbb7bf21c44782fc7526fcfd196e6
Tatiana <-->
2023-07-03 18:22:48 +0300

    fx select edit popover

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=a2ec0f9"></script>
+    <script src="/js/main.js?v=3655808"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?4"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

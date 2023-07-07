8c9bc109dc9618fe28ac2da34727b092b365f7d8
Tatiana <-->
2023-07-07 12:31:51 +0300

    rows csv import with file field

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=8d44c79">
-    <script src="/js/main.js?v=f272bf6"></script>
+    <script src="/js/main.js?v=8bc549b"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?5"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

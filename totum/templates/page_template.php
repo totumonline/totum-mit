aa71d6dab398c00fac7c94c5f5a100e1b37dc721
Tatiana <-->
2023-07-07 11:17:33 +0300

    attention note when panels max count limit is exceeded

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,8 +18,8 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=cc84f87"></script>
-    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?4"></script>
+    <script src="/js/main.js?v=3b4292a"></script>
+    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?5"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,8 +8,8 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=8d44c79">
-    <script src="/js/main.js?v=cc84f87"></script>
-    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?4"></script>
+    <script src="/js/main.js?v=3b4292a"></script>
+    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?5"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>

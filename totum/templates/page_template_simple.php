58b7115815a58f00871f90d46fe8c20c8de36dac
Tatiana <-->
2023-07-10 17:56:19 +0300

    display topbutton:false buttons in CreatorView

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=bb9fb0a">
-    <script src="/js/main.js?v=82b43e4"></script>
+    <script src="/js/main.js?v=7a4a1c5"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?5"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

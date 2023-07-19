4edeaa3a5dec48692076dbd673d39faad6b55772
Tatiana <-->
2023-07-19 15:36:45 +0300

    fx dotbuttons

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=ac133f7"></script>
+    <script src="/js/main.js?v=001fb1b"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?5"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=fd5e453">
-    <script src="/js/main.js?v=ac133f7"></script>
+    <script src="/js/main.js?v=001fb1b"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?5"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

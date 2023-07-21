34a57bbf616678567f4bd16b2a96a4a7906bd08b
Tatiana <-->
2023-07-21 13:02:12 +0300

    fx colorizer

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=001fb1b"></script>
+    <script src="/js/main.js?v=998a936"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?5"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

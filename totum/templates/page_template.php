0dbf18dcc204cda87015212c319a4b777f8bb312
Tatiana <-->
2023-07-25 16:30:37 +0300

    servicesAskOpenai

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -11,7 +11,7 @@
 
     <?php
     if ($isCreatorView ?? null) { ?>
-        <script src="/js/functions.js?v=2a8c3b0"></script>
+        <script src="/js/functions.js?v=67bc3ca"></script>
         <?php
         echo '<script>App.functions=App.functions.concat(' . $this->Config->getExtFunctionsTemplates() . ')</script>';
         ?>

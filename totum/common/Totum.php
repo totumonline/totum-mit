d39b48695a7bbf947167a0d2ecf553c858551441
Tatiana <-->
2023-07-26 13:27:38 +0300

    4.10.55.7

--- totum/common/Totum.php
+++ totum/common/Totum.php
@@ -27,7 +27,7 @@ use totum\tableTypes\tmpTable;
  */
 class Totum
 {
-    public const VERSION = '4.10.55.6';
+    public const VERSION = '4.10.55.7';
 
 
     public const TABLE_CODE_PARAMS = ['row_format', 'table_format', 'on_duplicate', 'default_action'];

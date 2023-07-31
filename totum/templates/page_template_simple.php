e309eb350c2a9f70a2092b52e2c6e2d585ac80a6
Tatiana <-->
2023-07-31 12:04:46 +0300

    pointing in self/blank/top target - linktotable function

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=9bc166f">
-    <script src="/js/main.js?v=4a54260"></script>
+    <script src="/js/main.js?v=2a17ad6"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?6"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

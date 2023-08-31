3413054fd4b7cdb5356e683f828278df83b3dee3
Tatiana <-->
2023-08-31 14:18:42 +0300

    rm hide&width if topButtons:false

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=b263331"></script>
+    <script src="/js/main.js?v=1322d8d"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?9"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=7f3689c">
-    <script src="/js/main.js?v=b263331"></script>
+    <script src="/js/main.js?v=1322d8d"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?9"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

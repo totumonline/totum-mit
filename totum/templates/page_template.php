7902efaca0443307afcb14c1deb7daf976c7d719
Tatiana <-->
2023-09-05 17:37:20 +0300

    Running timeout up to 500 ms

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=07d4a9f"></script>
+    <script src="/js/main.js?v=775dad7"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?9"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

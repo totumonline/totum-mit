c6de0bce19e24f7932ad466e1bef620bd3be1247
Tatiana <-->
2023-09-06 15:19:51 +0300

    js translations up

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -19,7 +19,7 @@
     } ?>
 
     <script src="/js/main.js?v=775dad7"></script>
-    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?9"></script>
+    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?10"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
 

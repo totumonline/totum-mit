a9359a1cbe248cf344bbda43953b3b29f4ef8199
Tatiana <-->
2023-09-26 11:18:51 +0300

    fxs in InsertRow

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=1f0a57c">
-    <script src="/js/main.js?v=cecafaa"></script>
+    <script src="/js/main.js?v=19dce32"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?10"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

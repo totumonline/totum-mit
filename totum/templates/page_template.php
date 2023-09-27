e85333a3a3d909465abced9188c08265d5d455dd
Tatiana <-->
2023-09-27 13:39:44 +0300

    fxs insertRow Tabs

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=6fab080"></script>
+    <script src="/js/main.js?v=5454a3b"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?10"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

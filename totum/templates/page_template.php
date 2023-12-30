cffb80234fe311064900830fcdd058160893b787
Tatiana <-->
2023-12-30 15:02:34 +0300

    css upds

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=63416c5"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=70ff68c">
+          href="/css/main.css?v=9b3b642">
 
     <?php
     if ($isCreatorView ?? null) { ?>
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=63416c5"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=70ff68c">
+          href="/css/main.css?v=9b3b642">
     <script src="/js/main.js?v=dbe3e43"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?10"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>

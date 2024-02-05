4198aa44ca05c503c819658b4912f4a118d6f9f4
Tatiana <-->
2024-02-05 15:43:51 +0300

    fx reload after refresh

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=f7ecbad"></script>
+    <script src="/js/main.js?v=525b73d"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?10"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

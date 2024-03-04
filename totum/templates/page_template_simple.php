91496073e33a5996bbed2052ed55c08c8046cfa4
Tatiana <-->
2024-03-04 17:50:30 +0300

    fx button icon & comment error

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=3ce23fb">
-    <script src="/js/main.js?v=525b73d"></script>
+    <script src="/js/main.js?v=60345b2"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?10"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

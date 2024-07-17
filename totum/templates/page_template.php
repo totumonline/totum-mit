25705d9bf227997bfeb6831b9959f9142f48e9c1
Tatiana <-->
2024-07-17 14:31:34 +0300

    eraser in tab

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=60345b2"></script>
+    <script src="/js/main.js?v=9800d35"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?10"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

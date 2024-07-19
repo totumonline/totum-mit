434677e0cf982bcf2461b23c7724393f9108c594
Tatiana <-->
2024-07-19 13:16:03 +0300

    fx select add button in insert row in iframe

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=5ac0414"></script>
+    <script src="/js/main.js?v=573c966"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?10"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

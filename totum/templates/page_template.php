0c250ee0bc28ee9391f9b1b2588664a13caf0795
Tatiana <-->
2024-07-22 18:50:51 +0300

    function help in iframe

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=ae241cf"></script>
+    <script src="/js/main.js?v=e2c8439"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?10"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

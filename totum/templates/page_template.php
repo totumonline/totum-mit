b0159f13e91b86f79ae4bb18e83ebcbb3b8bec00
Tatiana <-->
2024-08-12 14:34:13 +0300

    css panels upds

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=d139c23"></script>
+    <script src="/js/main.js?v=d0cf38b"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?10"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=f630085">
-    <script src="/js/main.js?v=d139c23"></script>
+    <script src="/js/main.js?v=d0cf38b"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?10"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

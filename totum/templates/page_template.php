772573609112a73d899d851f053ddc4e7529be34
Tatiana <-->
2024-09-14 16:56:41 +0300

    linewrapping

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=ff3bda6"></script>
+    <script src="/js/main.js?v=078833e"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?10"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

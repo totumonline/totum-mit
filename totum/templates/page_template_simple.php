6292637265bdaf1cedb879d2300a9165c3a641f3
Tatiana <-->
2024-09-25 17:57:39 +0300

    fx mobile menu

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=b101ece"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=de85130">
-    <script src="/js/main.js?v=a92b961"></script>
+          href="/css/main.css?v=df6dd09">
+    <script src="/js/main.js?v=f453624"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?10"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

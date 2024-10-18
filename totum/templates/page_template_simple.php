ccc5039b1d0903875d5e0318bf90c149012e2215
Tatiana <-->
2024-10-18 13:13:42 +0300

    AI

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=a39a6aa">
-    <script src="/js/main.js?v=1be7afe"></script>
+    <script src="/js/main.js?v=3237062"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?10"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

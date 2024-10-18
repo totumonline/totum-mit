a113f231e31c10fa64109a4ddee9ece0b8728fad
Tatiana <-->
2024-10-18 14:45:29 +0300

    AI

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=b101ece"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=961fd73">
+          href="/css/main.css?v=a888344">
 
     <?php
     if ($isCreatorView ?? null) { ?>

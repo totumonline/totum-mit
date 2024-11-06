b866db620e2fc9d7893d88f0062d51222b50e6e6
Tatiana <-->
2024-11-06 12:17:04 +0300

    js langs upd

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -19,7 +19,7 @@
     } ?>
 
     <script src="/js/main.js?v=392e974"></script>
-    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?10"></script>
+    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?11"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -9,7 +9,7 @@
           type="text/css"
           href="/css/main.css?v=046f9e8">
     <script src="/js/main.js?v=392e974"></script>
-    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?10"></script>
+    <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?11"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
     <link rel="shortcut icon" type="image/png" href="/fls/6_favicon.png"/>

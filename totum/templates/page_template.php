19a116d2485d0d6efd181e4237266a220828d41d
Tatiana <-->
2024-11-06 08:58:01 +0300

    AI button to top

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=3a5eec8"></script>
+    <script src="/js/main.js?v=392e974"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?10"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

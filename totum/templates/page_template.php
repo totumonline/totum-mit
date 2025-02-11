a55281644001838e56efcdacd4a1cc79280e8ab1
Tatiana <-->
2025-02-11 14:34:00 +0300

    front insertRow fixes

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=12bc142"></script>
+    <script src="/js/main.js?v=c52def0"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?12"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

2c76d90d32942c8b75f0fa9f4e15865e0e026623
Tatiana <-->
2025-02-25 12:33:18 +0300

    insertRow front improves

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=2923e3e"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=3ef16c3">
+          href="/css/main.css?v=80acc60">
 
     <?php
     if ($isCreatorView ?? null) { ?>

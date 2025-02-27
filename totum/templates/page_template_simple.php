7a1163d7ad1620ddcd46d072e44ddf69434b48a7
Tatiana <-->
2025-02-27 10:34:38 +0300

    front imprvs

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=2923e3e"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=6661dac">
-    <script src="/js/main.js?v=a4d57d3"></script>
+          href="/css/main.css?v=de958ee">
+    <script src="/js/main.js?v=4fe6bad"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

6d72001452f30cc4c4443091c81436d7dd6e278c
Tatiana <-->
2025-03-13 11:09:54 +0300

    fx insertRow

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=556d990">
-    <script src="/js/main.js?v=dd8b014"></script>
+    <script src="/js/main.js?v=6f2f502"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

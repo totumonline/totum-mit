94082508e53bbcdadfaaa35536a2c410e73e7613
Tatiana <-->
2025-03-14 08:51:27 +0300

    front fxs

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=2923e3e"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=556d990">
-    <script src="/js/main.js?v=6f2f502"></script>
+          href="/css/main.css?v=9068143">
+    <script src="/js/main.js?v=f1a81a3"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

2191d0e49c16765ecfff6123427d77e3738305e0
Tatiana <-->
2025-04-16 09:07:04 +0300

    fx tree blinking

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=c7aa7d9"></script>
+    <script src="/js/main.js?v=db161d7"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

31cec34ff6d83de8c8fab2fca3cb409002e8a02c
Tatiana <-->
2025-04-21 12:50:42 +0300

    non-web field title click fx

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=db161d7"></script>
+    <script src="/js/main.js?v=6d7752f"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=f9a98f3">
-    <script src="/js/main.js?v=db161d7"></script>
+    <script src="/js/main.js?v=6d7752f"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

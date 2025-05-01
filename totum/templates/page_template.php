e4f35af3dff945451c12f14747e6b860a24c1f03
Tatiana <-->
2025-05-01 07:58:59 +0300

    summarizer currency view

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=2923e3e"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=f9a98f3">
+          href="/css/main.css?v=8b07ede">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=f644b3d"></script>
+    <script src="/js/main.js?v=7ecb54c"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

39c7a3bafa88821500d07ab06496975a4fa53be2
Tatiana <-->
2025-06-14 14:06:34 +0300

    fx modal-header select

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=2923e3e"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=55bd9d7">
+          href="/css/main.css?v=f456590">
 
     <?php
     if ($isCreatorView ?? null) { ?>
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=2923e3e"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=55bd9d7">
+          href="/css/main.css?v=f456590">
     <script src="/js/main.js?v=dc60d61"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>

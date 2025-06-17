6d48349c2f88c14c6a3d10399586ef1aa59a99b4
Tatiana <-->
2025-06-17 16:49:52 +0300

    fx float blocks height with noTitles

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=dc60d61"></script>
+    <script src="/js/main.js?v=dcd0290"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 
--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=f456590">
-    <script src="/js/main.js?v=dc60d61"></script>
+    <script src="/js/main.js?v=dcd0290"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

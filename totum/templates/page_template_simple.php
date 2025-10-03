d250cf90cb09c77277c019004c74016b9af4183b
Tatiana <-->
2025-10-03 16:05:00 +0300

    fieldSets upd

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,8 +7,8 @@
     <script src="/js/libs.js?v=2923e3e"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=56edc26">
-    <script src="/js/main.js?v=869218b"></script>
+          href="/css/main.css?v=9b58ba7">
+    <script src="/js/main.js?v=311d79b"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

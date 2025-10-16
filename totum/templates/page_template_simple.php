9d54b710d412b4d3f3cfc6cfeb3062427f9b2e58
Tatiana <-->
2025-10-16 16:25:27 +0300

    fx horizontal draggable

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=5ca8365">
-    <script src="/js/main.js?v=311d79b"></script>
+    <script src="/js/main.js?v=dc24455"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

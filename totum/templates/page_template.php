c496f982e6280202fbcf4cf490c6328f150571a4
Tatiana <-->
2025-10-21 15:16:40 +0300

    exclude,silent,actuality=off schema

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -11,7 +11,7 @@
 
     <?php
     if ($isCreatorView ?? null) { ?>
-        <script src="/js/functions.js?v=e36cbd7"></script>
+        <script src="/js/functions.js?v=aa7fba6"></script>
         <?php
         echo '<script>App.functions=App.functions.concat(' . $this->Config->getExtFunctionsTemplates() . ')</script>';
         ?>

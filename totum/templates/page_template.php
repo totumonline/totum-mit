55f4335ca9314fb4409f95ffb7e6248ab27109b9
Tatiana <-->
2025-11-04 13:34:27 +0300

    functions: getFromScript + request param

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -11,7 +11,7 @@
 
     <?php
     if ($isCreatorView ?? null) { ?>
-        <script src="/js/functions.js?v=aa7fba6"></script>
+        <script src="/js/functions.js?v=1db286a"></script>
         <?php
         echo '<script>App.functions=App.functions.concat(' . $this->Config->getExtFunctionsTemplates() . ')</script>';
         ?>

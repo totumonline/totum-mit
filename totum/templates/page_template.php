3bb73f20f36b60e8a05918c5daef90a7928b62e5
Tatiana <-->
2025-11-30 15:28:35 +0300

    emailSend description update for PRO:sptm

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -11,7 +11,7 @@
 
     <?php
     if ($isCreatorView ?? null) { ?>
-        <script src="/js/functions.js?v=1db286a"></script>
+        <script src="/js/functions.js?v=6d80399"></script>
         <?php
         echo '<script>App.functions=App.functions.concat(' . $this->Config->getExtFunctionsTemplates() . ')</script>';
         ?>

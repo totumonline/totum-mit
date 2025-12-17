62c5ce8883f672376baab55aa190361043730c63
Tatiana <-->
2025-12-17 18:58:54 +0300

    button clicks

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=6e7a686"></script>
+    <script src="/js/main.js?v=8cf457a"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

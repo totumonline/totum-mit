db5f118b21c3f24a4561d9a006c0424ce78235b2
Tatiana <-->
2025-12-19 11:49:18 +0300

    button clicks selected fx

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=ef6b332">
-    <script src="/js/main.js?v=8cf457a"></script>
+    <script src="/js/main.js?v=1fafb89"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

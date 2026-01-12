9b56e610bb84d1b56c7a1efab4312cb999d5f6b0
Tatiana <-->
2026-01-12 18:27:40 +0300

    front fxs

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=2923e3e"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=ef6b332">
+          href="/css/main.css?v=d5735ad">
 
     <?php
     if ($isCreatorView ?? null) { ?>
@@ -18,7 +18,7 @@
         <?php
     } ?>
 
-    <script src="/js/main.js?v=f0fa37d"></script>
+    <script src="/js/main.js?v=f88dc63"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

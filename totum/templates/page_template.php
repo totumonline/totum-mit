b608a69cf9f6d95423a7adaae43fbd898ce2266a
Tatiana <-->
2026-01-23 08:49:25 +0300

    proCookie

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -11,7 +11,7 @@
 
     <?php
     if ($isCreatorView ?? null) { ?>
-        <script src="/js/functions.js?v=fdb839d"></script>
+        <script src="/js/functions.js?v=60d0718"></script>
         <?php
         echo '<script>App.functions=App.functions.concat(' . $this->Config->getExtFunctionsTemplates() . ')</script>';
         ?>

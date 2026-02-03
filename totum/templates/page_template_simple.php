7b1380283c7d8ad585614629aa78136077fc3d01
Tatiana <-->
2026-02-03 17:39:22 +0300

    css upd

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=2923e3e"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=48d9327">
+          href="/css/main.css?v=8b17c18">
     <script src="/js/main.js?v=29fb704"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>

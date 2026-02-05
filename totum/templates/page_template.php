82488f189f03e8c823fb652aa95d6186cb16dd09
Tatiana <-->
2026-02-05 09:27:22 +0300

    css upd

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -7,7 +7,7 @@
     <script src="/js/libs.js?v=2923e3e"></script>
     <link rel="stylesheet"
           type="text/css"
-          href="/css/main.css?v=249375f">
+          href="/css/main.css?v=90201e6">
 
     <?php
     if ($isCreatorView ?? null) { ?>

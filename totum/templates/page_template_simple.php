c00d7d146390b7b64be830dec924c09f246a5951
Tatiana <-->
2026-04-24 15:01:39 +0300

    fx selectTableBaseField in insert row

--- totum/templates/page_template_simple.php
+++ totum/templates/page_template_simple.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet"
           type="text/css"
           href="/css/main.css?v=8d8b317">
-    <script src="/js/main.js?v=1daf183"></script>
+    <script src="/js/main.js?v=0dd9afc"></script>
     <script src="/js/i18n/<?= $this->Config->getLang() ?>.js?13"></script>
     <script>App.lang = App.langs["<?= $this->Config->getLang() ?>"]</script>
 

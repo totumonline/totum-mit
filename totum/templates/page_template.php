ef2643e5b9f031f3be5df327dff94cbc6e78e3ba
Tatiana <-->
2026-05-13 15:33:56 +0300

    functions upd

--- totum/templates/page_template.php
+++ totum/templates/page_template.php
@@ -11,7 +11,7 @@
 
     <?php
     if ($isCreatorView ?? null) { ?>
-        <script src="/js/functions.js?v=6556610"></script>
+        <script src="/js/functions.js?v=50f5344"></script>
         <?php
         echo '<script>App.functions=App.functions.concat(' . $this->Config->getExtFunctionsTemplates() . ')</script>';
         ?>
